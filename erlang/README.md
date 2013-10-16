21231b308715140494741ea8e50374c341c2002d
Terence Parr <parrt@antlr.org>
2013-10-16 14:12:01 -0700

    add make stuff from original bit bucket https://bitbucket.org/fenollp/erlang-grammar

---
+++ erlang/README.md
@@ -0,0 +1,34 @@
+#erlang-grammar • [Bitbucket](https://bitbucket.org/fenollp/erlang-grammar.git)
+
+# Overview
+
+An unofficial Erlang grammar adaptation in **ANTLR v4**.
+Typically, the only one on the web.
+
+It is merely extracted from the [official non-standard grammar from Erlang/OTP sources](https://github.com/erlang/otp/blob/maint/lib/stdlib/src/erl_parse.yrl).
+
+It is meant for students and people who work on Erlang's syntax.
+
+
+## Requierements
+* [antlr4](http://www.antlr.org/wiki/display/ANTLR4/Getting+Started+with+ANTLR+v4)
+* Erlang's **erlc** for preprocessing purposes.
+
+# Usage
+
+Generate the grammar from Erlang.g4 with:
+
+    :::bash
+    make clean all
+
+Live debug with tree representation:
+
+    :::bash
+    make debug
+    …type Erlang forms…
+    ^D
+
+Check Erlang.g4 against the whole Erlang/OTP's `.erl` sources:
+
+    :::bash
+    make check
---
+++ erlang/check.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Test Erlang.g4 grammar against Erlang/OTP's *.erl sources
+# Note: a preprocessing pass has to be done beforehand (creates *.P files)
+
+get() {
+    URL="$1" ; SRC="$2"
+    which wget 1>/dev/null 2>/dev/null
+    if [ $? -eq 0 ]; then
+        wget $URL -O maint
+    else
+        curl -O $URL
+    fi
+    unzip maint
+    rm -f maint
+    mv otp-maint/ $SRC/
+}
+
+
+SRC="$1"
+
+[[ ! -d $SRC/ ]] && get 'https://codeload.github.com/erlang/otp/zip/maint' $SRC
+
+O=$SRC/output.txt
+tot=$(echo $(find $SRC -name '*.erl' | wc -l))
+echo -n "${tot} files to test. (stop with both ^C and ^G). SKIPPED <> preprocessing failed. "
+[[ $tot -gt 100 ]] && printf "\e[1;4m%s\e[0m" 'This will take a while!'
+[[ $tot -eq   0 ]] && printf "\e[1;4m%s\n\e[0m" "No $SRC/'*.erl' files to check on!" && exit 1
+echo
+
+j=$tot ; oks=0 ; skd=0
+for file in `find $SRC -name '*.erl'`; do
+    ## Remove random appearances of ‘’.
+    #sed -i 's///g' $file
+    COLUMNS=$(tput cols)
+    dir=`dirname $file`
+
+    pped=${file%.erl}.P
+    ## Preprocessor
+    erlc -I $dir/../include -I ${file%.erl}_data/ -o $dir -P $file 1>/dev/null 2>/dev/null
+    if [ $? -eq 0 ]; then
+
+        ## Syntax
+        java org.antlr.v4.runtime.misc.TestRig Erlang forms -encoding utf8 $pped 1>$O 2>>$O
+
+        if [ -s $O ]; then # $O is not empty
+            printf "%s \033[0;33m%s\033[00m\033[0;31m%$(($COLUMNS - ${#file} -${#j} -1))s\033[00m\n" $j $pped ERROR
+            cat $O
+        else # $O is empty
+            printf "%s \033[0;33m%s\033[00m\033[0;32m%$(($COLUMNS - ${#file} -${#j} -1))s\033[00m\n" $j $pped PASSED
+            ((oks++))
+        fi
+
+    else # Preprocessing failed
+        # Reasons: some of the files to include are not built yet (C code…) ; -I option is wrong (unlikely).
+        printf "%s \033[0;33m%s\033[00m\033[0;34m%$(($COLUMNS - ${#file} -${#j} -1))s\033[00m\n" $j $file SKIPPED
+        ((skd++))
+        ((oks++))
+    fi
+    ((j--))
+done
+rm -f $O
+echo
+
+lhs="$(($tot - $oks)) errors, checked: $tot, skipped: $skd"
+printf "\e[1;4m%s\e[0m\033[1;1m%$(($COLUMNS - ${#lhs}))s\033[00m\n" "$lhs" "$((100 * $oks / $tot))%"
