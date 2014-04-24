77aa514c2f1d8f8fa6445f8c22bc97e8223e9fc8
Vlad Shlosberg <vshlosbe@linkedin.com>
2014-04-24 03:23:53 -0700

    conditionals

---
+++ scss/src/test/java/TestConditionals.java
@@ -0,0 +1,55 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestConditionals extends TestBase
+{
+  @Test
+  public void testIf()
+  {
+    String [] lines = {
+        "@if 1 + 1 == 2 {}"
+    };
+    ScssParser.IfDeclarationContext context = parse(lines).statement(0).ifDeclaration();
+    Assert.assertEquals(context.conditions().condition().commandStatement().expression().getText(), "1");
+    Assert.assertEquals(context.conditions().condition().commandStatement().mathCharacter().getText(), "+");
+    Assert.assertEquals(context.conditions().condition().commandStatement().commandStatement().expression().getText(), "1");
+
+    Assert.assertEquals(context.conditions().condition().conditions().condition().commandStatement().expression().getText()
+        , "2");
+
+
+  }
+
+  @Test
+  public void testElseIf()
+  {
+    String [] lines = {
+        "@if 1 + 1 == 2 {}",
+        "@else if 1 + 1 == 3 {}"
+    };
+    ScssParser.ElseIfStatementContext context = parse(lines).statement(0).ifDeclaration().elseIfStatement(0);
+    Assert.assertEquals(context.conditions().condition().commandStatement().expression().getText(), "1");
+    Assert.assertEquals(context.conditions().condition().commandStatement().mathCharacter().getText(), "+");
+    Assert.assertEquals(context.conditions().condition().commandStatement().commandStatement().expression().getText(), "1");
+
+    Assert.assertEquals(context.conditions().condition().conditions().condition().commandStatement().expression().getText()
+        , "3");
+  }
+
+
+  @Test
+  public void testElseIfElse()
+  {
+    String [] lines = {
+        "@if 1 + 1 == 2 {}",
+        "@else if 1 + 1 == 3 {}",
+        "@else { color: red }"
+
+    };
+    ScssParser.ElseStatementContext context = parse(lines).statement(0).ifDeclaration().elseStatement();
+    Assert.assertEquals(context.block().property(0).identifier().getText(), "color");
+    Assert.assertEquals(context.block().property(0).value().getText(), "red");
+
+  }
+}
