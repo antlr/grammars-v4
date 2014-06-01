5187946a00762f1cbc66465c3976d56a40080743
Tom Everett <tom@khubla.com>
2014-06-01 17:19:34 -0600

    ugh

--- support/bnf2antlr/src/test/java/com/khubla/bnf2antlr/TestBNFLexerParser.java
+++ support/bnf2antlr/src/test/java/com/khubla/bnf2antlr/TestBNFLexerParser.java
@@ -12,21 +12,9 @@ import com.khubla.bnf.bnfParser.RulelistContext;
  */
 public class TestBNFLexerParser {
    @Test
-   public void testParser1() {
+   public void testParser() {
       try {
          final InputStream inputStream = TestBNFLexerParser.class.getResourceAsStream("/examples/pascal.bnf");
-         final RulelistContext rulelistContext = BNFDocumentParser.parse(inputStream);
-         Assert.assertNotNull(rulelistContext);
-      } catch (final Exception e) {
-         e.printStackTrace();
-         Assert.fail();
-      }
-   }
-
-   @Test
-   public void testParser2() {
-      try {
-         final InputStream inputStream = TestBNFLexerParser.class.getResourceAsStream("/examples/algol60.bnf");
          final RulelistContext rulelistContext = BNFDocumentParser.parse(inputStream);
          Assert.assertNotNull(rulelistContext);
       } catch (final Exception e) {
