430e46600f9183a08b046a062d4952ac5ba1b827
Tom Everett <tom@khubla.com>
2014-05-31 18:56:54 -0600

    fix

--- support/bnf2antlr/src/test/java/com/khubla/bnf2antlr/TestBNFLexerParser.java
+++ support/bnf2antlr/src/test/java/com/khubla/bnf2antlr/TestBNFLexerParser.java
@@ -13,7 +13,7 @@ public class TestBNFLexerParser {
    @Test
    public void testParser() {
       try {
-         final InputStream inputStream = TestBNFLexerParser.class.getResourceAsStream("/pascal.bnf");
+         final InputStream inputStream = TestBNFLexerParser.class.getResourceAsStream("/examples/pascal.bnf");
          final RulelistContext rulelistContext = BNFDocumentParser.parse(inputStream);
          Assert.assertNotNull(rulelistContext);
       } catch (final Exception e) {
