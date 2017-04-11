5f7c748d396d9ff121963672481e6fdf7b1aae94
Kevin RAMAGE <kevinramage@hotmail.fr>
2017-04-11 11:11:24 +0200

    Add Open XML statement

--- tsql/examples/dml_select.sql
+++ tsql/examples/dml_select.sql
@@ -636,3 +636,61 @@ SELECT ProductID, a.Name, Color
 FROM Production.Product AS a  
 INNER JOIN (VALUES ('Blade'), ('Crown Race'), ('AWC Logo Cap')) AS b(Name)   
 ON a.Name = b.Name;  
+
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- Open XML Statement
+
+
+DECLARE @idoc int, @doc varchar(1000);  
+SET @doc ='  
+<ROOT>  
+<Customer CustomerID="VINET" ContactName="Paul Henriot">  
+   <Order CustomerID="VINET" EmployeeID="5" OrderDate="1996-07-04T00:00:00">  
+      <OrderDetail OrderID="10248" ProductID="11" Quantity="12"/>  
+      <OrderDetail OrderID="10248" ProductID="42" Quantity="10"/>  
+   </Order>  
+</Customer>  
+<Customer CustomerID="LILAS" ContactName="Carlos Gonzlez">  
+   <Order CustomerID="LILAS" EmployeeID="3" OrderDate="1996-08-16T00:00:00">  
+      <OrderDetail OrderID="10283" ProductID="72" Quantity="3"/>  
+   </Order>  
+</Customer>  
+</ROOT>';  
+--Create an internal representation of the XML document.  
+EXEC sp_xml_preparedocument @idoc OUTPUT, @doc;  
+-- Execute a SELECT statement that uses the OPENXML rowset provider.  
+SELECT    *  
+FROM       OPENXML (@idoc, '/ROOT/Customer',1)  
+            WITH (CustomerID  varchar(10),  
+                  ContactName varchar(20));  
+				  
+
+				  DECLARE @idoc int, @doc varchar(1000);   
+SET @doc ='  
+<ROOT>  
+<Customer CustomerID="VINET" ContactName="Paul Henriot">  
+   <Order OrderID="10248" CustomerID="VINET" EmployeeID="5"   
+           OrderDate="1996-07-04T00:00:00">  
+      <OrderDetail ProductID="11" Quantity="12"/>  
+      <OrderDetail ProductID="42" Quantity="10"/>  
+   </Order>  
+</Customer>  
+<Customer CustomerID="LILAS" ContactName="Carlos Gonzlez">v  
+   <Order OrderID="10283" CustomerID="LILAS" EmployeeID="3"   
+           OrderDate="1996-08-16T00:00:00">  
+      <OrderDetail ProductID="72" Quantity="3"/>  
+   </Order>  
+</Customer>  
+</ROOT>';   
+  
+--Create an internal representation of the XML document.  
+EXEC sp_xml_preparedocument @idoc OUTPUT, @doc;   
+  
+-- SELECT stmt using OPENXML rowset provider  
+SELECT *  
+FROM   OPENXML (@idoc, '/ROOT/Customer/Order/OrderDetail',2)   
+         WITH (OrderID       int         '../@OrderID',   
+               CustomerID  varchar(10) '../@CustomerID',   
+               OrderDate   datetime    '../@OrderDate',   
+               ProdID      int         '@ProductID',   
+               Qty         int         '@Quantity');  
