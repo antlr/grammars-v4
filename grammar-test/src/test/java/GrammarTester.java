f6757f91f634149f11d456dc8c1c8a4e0b2bac0c
Julian Thome <julian.thome@uni.lu>
2017-06-12 10:47:13 +0200

    grammar tester

--- grammar-test/src/test/java/GrammarTester.java
+++ grammar-test/src/test/java/GrammarTester.java
@@ -4,9 +4,14 @@ import org.snt.inmemantlr.GenericParser;
 import org.snt.inmemantlr.exceptions.CompilationException;
 import org.snt.inmemantlr.exceptions.IllegalWorkflowException;
 import org.snt.inmemantlr.listener.DefaultTreeListener;
+import org.snt.inmemantlr.tree.Ast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GrammarTester {
 
@@ -46,15 +51,44 @@ public class GrammarTester {
                 try {
                     gp.parse(f);
                 } catch (FileNotFoundException e) {
-
+                    LOGGER.error(e.getMessage());
+                    return false;
                 }
             } catch (IllegalWorkflowException e) {
                 LOGGER.error(e.getMessage());
                 return false;
             }
         }
-
         return true;
+    }
+
+    public static Map<String, Ast> runAndGetAsts(File[] ok, File... gfile) {
+        GenericParser gp = create(gfile);
+
+        Map<String,Ast> ret = new HashMap<String, Ast>();
+
+        for (File f : ok) {
+            LOGGER.info("parse {}", f.getAbsoluteFile());
+            try {
+                try {
+                    gp.parse(f);
+                } catch (FileNotFoundException e) {
+                    LOGGER.error(e.getMessage());
+                    return null;
+                }
+            } catch (IllegalWorkflowException e) {
+                LOGGER.error(e.getMessage());
+                return null;
+            }
+
+            Path p = Paths.get(f.toURI());
+
+            DefaultTreeListener l = (DefaultTreeListener)gp.getListener();
+
+            ret.put(p.getFileName().toString(), l.getAst());
+        }
+
+        return ret;
     }
 
     public static boolean run(File... gfile) {
