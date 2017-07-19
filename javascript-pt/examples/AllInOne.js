d0ef1f1b40bd79928331b6f3d1cf1b9cf025c437
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-19 20:27:53 +0300

    AllInOne.js extended. Added Internationalization & Localization:
    Collation;
    Number Formatting;
    Currency Formatting;
    Data/Time Formatting.

--- javascript-pt/examples/AllInOne.js
+++ javascript-pt/examples/AllInOne.js
@@ -818,3 +818,46 @@ let obj = { a: 1 }
 Object.defineProperty(obj, "b", { value: 2 })
 obj[Symbol("c")] = 3
 Reflect.ownKeys(obj) // [ "a", "b", Symbol(c) ]
+
+//--------------------------
+// Internationalization & Localization
+//--------------------------
+
+// Collation
+// http://es6-features.org/#Collation
+
+// in German,  "ä" sorts with "a"
+// in Swedish, "ä" sorts after "z"
+var list = [ "ä", "a", "z" ]
+var l10nDE = new Intl.Collator("de")
+var l10nSV = new Intl.Collator("sv")
+l10nDE.compare("ä", "z") === -1
+l10nSV.compare("ä", "z") === +1
+console.log(list.sort(l10nDE.compare)) // [ "a", "ä", "z" ]
+console.log(list.sort(l10nSV.compare)) // [ "a", "z", "ä" ]
+
+// Number Formatting
+// http://es6-features.org/#NumberFormatting
+
+var l10nEN = new Intl.NumberFormat("en-US")
+var l10nDE = new Intl.NumberFormat("de-DE")
+l10nEN.format(1234567.89) === "1,234,567.89"
+l10nDE.format(1234567.89) === "1.234.567,89"
+
+// Currency Formatting
+// http://es6-features.org/#CurrencyFormatting
+
+var l10nUSD = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" })
+var l10nGBP = new Intl.NumberFormat("en-GB", { style: "currency", currency: "GBP" })
+var l10nEUR = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" })
+l10nUSD.format(100200300.40) === "$100,200,300.40"
+l10nGBP.format(100200300.40) === "£100,200,300.40"
+l10nEUR.format(100200300.40) === "100.200.300,40 €"
+
+// Date/Time Formatting
+// http://es6-features.org/#DateTimeFormatting
+
+var l10nEN = new Intl.DateTimeFormat("en-US")
+var l10nDE = new Intl.DateTimeFormat("de-DE")
+l10nEN.format(new Date("2015-01-02")) === "1/2/2015"
+l10nDE.format(new Date("2015-01-02")) === "2.1.2015"
