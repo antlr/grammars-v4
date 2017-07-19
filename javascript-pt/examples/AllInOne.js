70416953740b0e7d59374780e8bae0250e917948
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-19 18:24:49 +0300

    AllInOne.js extended (Class Getter/Setter).

--- javascript-pt/examples/AllInOne.js
+++ javascript-pt/examples/AllInOne.js
@@ -391,3 +391,20 @@ class Circle extends Shape {
 }
 var defRectangle = Rectangle.defaultRectangle()
 var defCircle    = Circle.defaultCircle()
+
+// Getter/Setter
+// http://es6-features.org/#GetterSetter
+
+class Rectangle {
+    constructor (width, height) {
+        this._width  = width
+        this._height = height
+    }
+    set width  (width)  { this._width = width               }
+    get width  ()       { return this._width                }
+    set height (height) { this._height = height             }
+    get height ()       { return this._height               }
+    get area   ()       { return this._width * this._height }
+}
+var r = new Rectangle(50, 20)
+r.area === 1000
