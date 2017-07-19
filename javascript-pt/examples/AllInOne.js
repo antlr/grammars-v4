08785fee2c84d5749fe0c99350b4660438677815
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-19 20:15:21 +0300

    AllInOne.js extended. New Builld-in Methods:
    Array Element Finding;
    String Repeating;
    String Searching;
    Number Type Checking;
    Number Safety Checking;
    Number Comparison;
    Number Sign Determination.

--- javascript-pt/examples/AllInOne.js
+++ javascript-pt/examples/AllInOne.js
@@ -692,3 +692,63 @@ dest.quux === 0
 dest.foo  === 3
 dest.bar  === 2
 dest.baz  === 4
+
+// Array Element Finding
+// http://es6-features.org/#ArrayElementFinding
+
+[ 1, 3, 4, 2 ].find(x => x > 3) // 4
+[ 1, 3, 4, 2 ].findIndex(x => x > 3) // 2
+
+// String Repeating
+// http://es6-features.org/#StringRepeating
+
+" ".repeat(4 * depth)
+"foo".repeat(3)
+
+// String Searching
+// http://es6-features.org/#StringSearching
+
+"hello".startsWith("ello", 1) // true
+"hello".endsWith("hell", 4)   // true
+"hello".includes("ell")       // true
+"hello".includes("ell", 1)    // true
+"hello".includes("ell", 2)    // false
+
+// Number Type Checking
+// http://es6-features.org/#NumberTypeChecking
+
+Number.isNaN(42) === false
+Number.isNaN(NaN) === true
+
+Number.isFinite(Infinity) === false
+Number.isFinite(-Infinity) === false
+Number.isFinite(NaN) === false
+Number.isFinite(123) === true
+
+// Number Safety Checking
+// http://es6-features.org/#NumberSafetyChecking
+
+Number.isSafeInteger(42) === true
+Number.isSafeInteger(9007199254740992) === false
+
+// Number Comparison
+// http://es6-features.org/#NumberComparison
+
+console.log(0.1 + 0.2 === 0.3) // false
+console.log(Math.abs((0.1 + 0.2) - 0.3) < Number.EPSILON) // true
+
+// Number Truncation
+// http://es6-features.org/#NumberTruncation
+
+console.log(Math.trunc(42.7)) // 42
+console.log(Math.trunc( 0.1)) // 0
+console.log(Math.trunc(-0.1)) // -0
+
+// Number Sign Determination
+// http://es6-features.org/#NumberSignDetermination
+
+console.log(Math.sign(7))   // 1
+console.log(Math.sign(0))   // 0
+console.log(Math.sign(-0))  // -0
+console.log(Math.sign(-7))  // -1
+console.log(Math.sign(NaN)) // NaN
