5b7b75fe7c4a8d644ab8d921c19e24ca0c79b5e0
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-19 18:26:31 +0300

    AllInOne.js extended (Global Symbols).

--- javascript-pt/examples/AllInOne.js
+++ javascript-pt/examples/AllInOne.js
@@ -428,3 +428,21 @@ JSON.stringify(obj) // {}
 Object.keys(obj) // []
 Object.getOwnPropertyNames(obj) // []
 Object.getOwnPropertySymbols(obj) // [ foo, bar ]
+
+// Global Symbols
+// http://es6-features.org/#GlobalSymbols
+
+Symbol.for("app.foo") === Symbol.for("app.foo")
+const foo = Symbol.for("app.foo")
+const bar = Symbol.for("app.bar")
+Symbol.keyFor(foo) === "app.foo"
+Symbol.keyFor(bar) === "app.bar"
+typeof foo === "symbol"
+typeof bar === "symbol"
+let obj = {}
+obj[foo] = "foo"
+obj[bar] = "bar"
+JSON.stringify(obj) // {}
+Object.keys(obj) // []
+Object.getOwnPropertyNames(obj) // []
+Object.getOwnPropertySymbols(obj) // [ foo, bar ]
