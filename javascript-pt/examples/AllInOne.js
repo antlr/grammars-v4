a17467c5e5d85168a661cfb6680c18268862d580
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-19 18:47:14 +0300

    AllInOne.js extended (Iterators & Generators).

--- javascript-pt/examples/AllInOne.js
+++ javascript-pt/examples/AllInOne.js
@@ -446,3 +446,145 @@ JSON.stringify(obj) // {}
 Object.keys(obj) // []
 Object.getOwnPropertyNames(obj) // []
 Object.getOwnPropertySymbols(obj) // [ foo, bar ]
+
+//--------------------------
+// TODO: Iterators
+//--------------------------
+
+// Iterator & For-Of Operator
+// http://es6-features.org/#IteratorForOfOperator
+
+/*let fibonacci = {
+    [Symbol.iterator]() {
+        let pre = 0, cur = 1
+        return {
+           next () {
+               [ pre, cur ] = [ cur, pre + cur ]
+               return { done: false, value: cur }
+           }
+        }
+    }
+}
+
+for (let n of fibonacci) {
+    if (n > 1000)
+        break
+    console.log(n)
+}*/
+
+//--------------------------
+// TODO: Generators
+//--------------------------
+
+// Generator Function, Iterator Protocol
+// http://es6-features.org/#GeneratorFunctionIteratorProtocol
+
+/*let fibonacci = {
+    *[Symbol.iterator]() {
+        let pre = 0, cur = 1
+        for (;;) {
+            [ pre, cur ] = [ cur, pre + cur ]
+            yield cur
+        }
+    }
+}
+
+for (let n of fibonacci) {
+    if (n > 1000)
+        break
+    console.log(n)
+}
+
+// Generator Function, Direct Use
+// http://es6-features.org/#GeneratorFunctionDirectUse
+
+function* range (start, end, step) {
+    while (start < end) {
+        yield start
+        start += step
+    }
+}
+
+for (let i of range(0, 10, 2)) {
+    console.log(i) // 0, 2, 4, 6, 8
+}
+
+// Generator Matching
+// http://es6-features.org/#GeneratorMatching
+
+let fibonacci = function* (numbers) {
+    let pre = 0, cur = 1
+    while (numbers-- > 0) {
+        [ pre, cur ] = [ cur, pre + cur ]
+        yield cur
+    }
+}
+
+for (let n of fibonacci(1000))
+    console.log(n)
+
+let numbers = [ ...fibonacci(1000) ]
+
+let [ n1, n2, n3, ...others ] = fibonacci(1000)
+
+// Generator Control-Flow
+// http://es6-features.org/#GeneratorControlFlow
+
+//  generic asynchronous control-flow driver
+function async (proc, ...params) {
+    var iterator = proc(...params)
+    return new Promise((resolve, reject) => {
+        let loop = (value) => {
+            let result
+            try {
+                result = iterator.next(value)
+            }
+            catch (err) {
+                reject(err)
+            }
+            if (result.done)
+                resolve(result.value)
+            else if (   typeof result.value      === "object"
+                     && typeof result.value.then === "function")
+                result.value.then((value) => {
+                    loop(value)
+                }, (err) => {
+                    reject(err)
+                })
+            else
+                loop(result.value)
+        }
+        loop()
+    })
+}
+
+//  application-specific asynchronous builder
+function makeAsync (text, after) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(text), after)
+    })
+}
+
+//  application-specific asynchronous procedure
+async(function* (greeting) {
+    let foo = yield makeAsync("foo", 300)
+    let bar = yield makeAsync("bar", 200)
+    let baz = yield makeAsync("baz", 100)
+    return `${greeting} ${foo} ${bar} ${baz}`
+}, "Hello").then((msg) => {
+    console.log("RESULT:", msg) // "Hello foo bar baz"
+})
+
+// Generator Methods
+// http://es6-features.org/#GeneratorMethods
+
+class Clz {
+    * bar () {
+        // …
+    }
+}
+let Obj = {
+    * foo () {
+        // …
+    }
+}*/
