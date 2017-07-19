528ab0c6721f7a78c8ada18e5cb7d24ed26bd5ee
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-19 18:18:20 +0300

    AllInOne.js extended (Class Inheritance, From Expressions).

--- javascript-pt/examples/AllInOne.js
+++ javascript-pt/examples/AllInOne.js
@@ -291,3 +291,57 @@ class Circle extends Shape {
     }
 }
 
+// Class Inheritance, From Expressions
+// http://es6-features.org/#ClassInheritanceFromExpressions
+
+var aggregation = (baseClass, ...mixins) => {
+    let base = class _Combined extends baseClass {
+        constructor (...args) {
+            super(...args)
+            mixins.forEach((mixin) => {
+                mixin.prototype.initializer.call(this)
+            })
+        }
+    }
+    let copyProps = (target, source) => {
+        Object.getOwnPropertyNames(source)
+            .concat(Object.getOwnPropertySymbols(source))
+            .forEach((prop) => {
+            if (prop.match(/^(?:constructor|prototype|arguments|caller|name|bind|call|apply|toString|length)$/))
+                return
+            Object.defineProperty(target, prop, Object.getOwnPropertyDescriptor(source, prop))
+        })
+    }
+    mixins.forEach((mixin) => {
+        copyProps(base.prototype, mixin.prototype)
+        copyProps(base, mixin)
+    })
+    return base
+}
+
+class Colored {
+    initializer ()     { this._color = "white" }
+    get color ()       { return this._color }
+    set color (v)      { this._color = v }
+}
+
+class ZCoord {
+    initializer ()     { this._z = 0 }
+    get z ()           { return this._z }
+    set z (v)          { this._z = v }
+}
+
+class Shape {
+    constructor (x, y) { this._x = x; this._y = y }
+    get x ()           { return this._x }
+    set x (v)          { this._x = v }
+    get y ()           { return this._y }
+    set y (v)          { this._y = v }
+}
+
+class Rectangle extends aggregation(Shape, Colored, ZCoord) {}
+
+var rect = new Rectangle(7, 42)
+rect.z     = 1000
+rect.color = "red"
+console.log(rect.x, rect.y, rect.z, rect.color)
