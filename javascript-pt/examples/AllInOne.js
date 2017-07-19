9b28a3d478157df207143470c48026180b7f818e
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-19 20:11:38 +0300

    AllInOne.js extended (Weak-Link Data-Structures).

--- javascript-pt/examples/AllInOne.js
+++ javascript-pt/examples/AllInOne.js
@@ -614,3 +614,32 @@ m.get(s) === 34
 m.size === 2
 for (let [ key, val ] of m.entries())
     console.log(key + " = " + val)
+
+// Weak-Link Data-Structures
+// http://es6-features.org/#WeakLinkDataStructures
+
+let isMarked     = new WeakSet()
+let attachedData = new WeakMap()
+
+export class Node {
+    constructor (id)   { this.id = id                  }
+    mark        ()     { isMarked.add(this)            }
+    unmark      ()     { isMarked.delete(this)         }
+    marked      ()     { return isMarked.has(this)     }
+    set data    (data) { attachedData.set(this, data)  }
+    get data    ()     { return attachedData.get(this) }
+}
+
+let foo = new Node("foo")
+
+JSON.stringify(foo) === '{"id":"foo"}'
+foo.mark()
+foo.data = "bar"
+foo.data === "bar"
+JSON.stringify(foo) === '{"id":"foo"}'
+
+isMarked.has(foo)     === true
+attachedData.has(foo) === true
+foo = null  /* remove only reference to foo */
+attachedData.has(foo) === false
+isMarked.has(foo)     === false
