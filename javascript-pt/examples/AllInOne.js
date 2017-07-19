7d0d2ec93a8b5a75d34c43587a21dcf855a16994
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-19 20:23:30 +0300

    AllInOne.js extended: Added Meta-Programming:
    Proxying;
    Reflection.

--- javascript-pt/examples/AllInOne.js
+++ javascript-pt/examples/AllInOne.js
@@ -792,3 +792,29 @@ Promise.all([
 }, (err) => {
     console.log(`error: ${err}`)
 })
+
+//--------------------------
+// Meta-Programming
+//--------------------------
+
+// Proxying
+// http://es6-features.org/#Proxying
+
+let target = {
+    foo: "Welcome, foo"
+}
+let proxy = new Proxy(target, {
+    get (receiver, name) {
+        return name in receiver ? receiver[name] : `Hello, ${name}`
+    }
+})
+proxy.foo   === "Welcome, foo"
+proxy.world === "Hello, world"
+
+// Reflection
+// http://es6-features.org/#Reflection
+
+let obj = { a: 1 }
+Object.defineProperty(obj, "b", { value: 2 })
+obj[Symbol("c")] = 3
+Reflect.ownKeys(obj) // [ "a", "b", Symbol(c) ]
