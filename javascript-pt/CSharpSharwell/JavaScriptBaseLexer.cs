6d54b15d52fae74bd3f57e4c81eb8bac6c843b94
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-21 17:31:26 +0300

    Correct processing of 'use strict' instructions, added StrictFunctions.js and StrictGlobal.js.

--- javascript-pt/CSharpSharwell/JavaScriptBaseLexer.cs
+++ javascript-pt/CSharpSharwell/JavaScriptBaseLexer.cs
@@ -1,10 +1,14 @@
 using Antlr4.Runtime;
+using System.Collections.Generic;
 using static PT.PM.JavaScriptParseTreeUst.Parser.JavaScriptParser;
 
 public abstract class JavaScriptBaseLexer : Lexer
 {
+    private Stack<bool> scopeStrictModes = new Stack<bool>();
     // The most recently produced token.
     private IToken _lastToken = null;
+    private bool _useStrictDefault = false;
+    private bool _useStrictCurrent = false;
 
     public JavaScriptBaseLexer(ICharStream input)
         : base(input)
@@ -14,11 +18,22 @@ public abstract class JavaScriptBaseLexer : Lexer
     // A property indicating if the lexer should operate in strict mode.
     // When set to true, FutureReservedWords are tokenized, when false,
     // an octal literal can be tokenized.
-    public bool UseStrict { get; set; }
+    public bool UseStrictDefalult
+    {
+        get
+        {
+            return _useStrictDefault;
+        }
+        set
+        {
+            _useStrictDefault = value;
+            _useStrictCurrent = value;
+        }
+    }
 
     public bool IsSrictMode()
     {
-        return UseStrict;
+        return _useStrictCurrent;
     }
 
     ///<summary>Return the next token from the character stream and records this last
@@ -30,6 +45,25 @@ public abstract class JavaScriptBaseLexer : Lexer
     {
         // Get the next token.
         IToken next = base.NextToken();
+
+        if (next.Type == OpenBrace)
+        {
+            _useStrictCurrent = scopeStrictModes.Count > 0 && scopeStrictModes.Peek() ? true : UseStrictDefalult;
+            scopeStrictModes.Push(_useStrictCurrent);
+        }
+        else if (next.Type == CloseBrace)
+        {
+            _useStrictCurrent = scopeStrictModes.Count > 0 ? scopeStrictModes.Pop() : UseStrictDefalult;
+        }
+        else if (next.Type == StringLiteral &&
+            (_lastToken == null || _lastToken.Type == OpenBrace) &&
+            (next.Text.Substring(1, next.Text.Length - 2)) == "use strict")
+        {
+            if (scopeStrictModes.Count > 0)
+                scopeStrictModes.Pop();
+            _useStrictCurrent = true;
+            scopeStrictModes.Push(_useStrictCurrent);
+        }
 
         if (next.Channel == Lexer.DefaultTokenChannel)
         {
