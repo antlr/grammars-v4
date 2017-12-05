a8033da706ceaf9f0473a9fabb52df055ba496ef
bedorlan <orlando.bedoya@openintl.com>
2017-12-05 12:11:17 -0500

    whitespace

--- plsql/examples-sql-script/pltables.sql
+++ plsql/examples-sql-script/pltables.sql
@@ -1,7 +1,7 @@
 declare
     TYPE tyrcmessage IS RECORD (id number, message varchar2(100));
     TYPE tytbMessages IS TABLE OF tyrcmessage INDEX BY BINARY_INTEGER;
-    
+
     tbmessages tytbMessages;
     nuidx      number;
 
