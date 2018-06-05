4284f27740937a78eb1a5c4fe2c74bbd80c7215d
Mark <mark@localhost.localdomain>
2018-06-05 00:09:19 -0400

    Added 10g possibility Added alter_database into mix.  Fixed a little grammar

--- plsql/Java/PlSqlBaseParser.java
+++ plsql/Java/PlSqlBaseParser.java
@@ -3,6 +3,7 @@ import org.antlr.v4.runtime.*;
 public abstract class PlSqlBaseParser extends Parser
 {
     private boolean _isVersion12 = true;
+    private boolean _isVersion10 = true;
 
     public PlSqlBaseParser(TokenStream input) {
         super(input);
@@ -14,5 +15,13 @@ public abstract class PlSqlBaseParser extends Parser
 
     public void setVersion12(boolean value) {
         _isVersion12 = value;
+    }
+
+    public boolean isVersion10() {
+        return _isVersion10;
+    }
+
+    public void setVersion10(boolean value) {
+        _isVersion10 = value;
     }
 }
