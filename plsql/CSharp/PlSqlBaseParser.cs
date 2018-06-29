cd53129ecefd992ade5a8fa8c5eb0e19cf014e43
livioc <livioc@noreply.github.com>
2018-06-29 16:00:03 +0200

    added 10g possibility to plsql / CSharp target

--- plsql/CSharp/PlSqlBaseParser.cs
+++ plsql/CSharp/PlSqlBaseParser.cs
@@ -2,14 +2,19 @@ using Antlr4.Runtime;
 
 public abstract class PlSqlBaseParser : Parser
 {
+    private bool _isVersion10 = false;
     private bool _isVersion12 = true;
 
-    public PlSqlBaseParser(ITokenStream input)
+    protected PlSqlBaseParser(ITokenStream input)
         : base(input)
     {
     }
 
+    public bool isVersion10() => _isVersion10;
+
     public bool isVersion12() => _isVersion12;
+
+    public bool setVersion10(bool value) => _isVersion10 = value;
 
     public bool setVersion12(bool value) => _isVersion12 = value;
 }
