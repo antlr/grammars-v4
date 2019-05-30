2d7e8db19544b21dac5f19ae06e748c40cb6de46
Bartosz Miedlar <bmiedlar@pgs-soft.com>
2019-05-30 12:08:07 +0200

    Allow backticks in string literals

--- mysql/examples/grant.sql
+++ mysql/examples/grant.sql
@@ -2,3 +2,6 @@ GRANT ALL ON tbl TO admin@localhost;
 GRANT ALL ON tbl TO admin;
 GRANT ALL PRIVILEGES ON tbl TO admin;
 GRANT ALL ON *.* TO admin;
+GRANT ALL ON *.* TO `admin`;
+GRANT ALL ON *.* TO 'admin';
+GRANT ALL ON *.* TO "admin";
