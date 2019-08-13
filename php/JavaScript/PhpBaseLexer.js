fdbb94943b85d222291c4b6cd3c3900357332979
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2019-08-13 19:03:21 +0200

    Restore / complete missing copyright headers

--- php/JavaScript/PhpBaseLexer.js
+++ php/JavaScript/PhpBaseLexer.js
@@ -1,9 +1,11 @@
 /*
  PHP grammar.
  
- The MIT License (MIT). Copyright (c) 2015-2017, Ivan Kochurkin (kvanttt@gmail.com), Positive
+ The MIT License (MIT).
+ Copyright (c) 2015-2017, Ivan Kochurkin (kvanttt@gmail.com), Positive
  Technologies.
- 
+ Copyright (c) 2019, Thierry Marianne (thierry.marianne@weaving-the-web.org)
+
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  associated documentation files (the "Software"), to deal in the Software without restriction,
  including without limitation the rights to use, copy, modify, merge, publish, distribute,
--- php/Python/PhpBaseLexer.py
+++ php/Python/PhpBaseLexer.py
@@ -3,6 +3,7 @@ PHP grammar.
 The MIT License (MIT).
 Copyright (c) 2015-2017, Ivan Kochurkin (kvanttt@gmail.com), Positive Technologies.
 Copyright (c) 2016, Jorrit Kronjee (Python port)
+Copyright (c) 2019, Thierry Marianne (thierry.marianne@weaving-the-web.org)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
