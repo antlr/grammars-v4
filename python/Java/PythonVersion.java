c43bb8ed949e90792539f4d82dc6d88aa0b5f8f4
Ivan Kochurkin <kvanttt@gmail.com>
2019-09-09 00:03:49 +0300

    Add Java runtime files (PythonLexerBase, PythonParserBase, PythonVersion)

---
+++ python/Java/PythonVersion.java
@@ -0,0 +1,17 @@
+package PythonParseTree;
+
+public enum PythonVersion {
+    Autodetect(0),
+    Python2(2),
+    Python3(3);
+
+    private final int value;
+
+    PythonVersion(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
