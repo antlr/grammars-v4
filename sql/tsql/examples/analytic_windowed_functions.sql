c791270add78870d5e6a5677ec9dc6fa024f0b50
siggemannen <6099273+siggemannen@users.noreply.github.com>
2020-10-03 22:07:49 +0200

    Update analytic_windowed_functions.sql

--- sql/tsql/examples/analytic_windowed_functions.sql
+++ sql/tsql/examples/analytic_windowed_functions.sql
@@ -15,3 +15,46 @@ FROM sales.SalesOrderHeader;
 
 SELECT LEAD(PurchaseOrderNumber) OVER(PARTITION BY CustomerID ORDER BY OrderDate) AS NextPONumber
 FROM sales.SalesOrderHeader;
+
+SELECT Department, LastName, Rate,   
+       CUME_DIST () OVER (PARTITION BY Department ORDER BY Rate) AS CumeDist,   
+       PERCENT_RANK() OVER (PARTITION BY Department ORDER BY Rate ) AS PctRank  
+FROM HumanResources.vEmployeeDepartmentHistory AS edh  
+    INNER JOIN HumanResources.EmployeePayHistory AS e    
+    ON e.BusinessEntityID = edh.BusinessEntityID  
+WHERE Department IN (N'Information Services',N'Document Control')   
+ORDER BY Department, Rate DESC;  
+
+SELECT DISTINCT Name AS DepartmentName  
+      ,PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY ph.Rate)   
+                            OVER (PARTITION BY Name) AS MedianCont  
+      ,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY ph.Rate)   
+                            OVER (PARTITION BY Name) AS MedianDisc  
+FROM HumanResources.Department AS d  
+INNER JOIN HumanResources.EmployeeDepartmentHistory AS dh   
+    ON dh.DepartmentID = d.DepartmentID  
+INNER JOIN HumanResources.EmployeePayHistory AS ph  
+    ON ph.BusinessEntityID = dh.BusinessEntityID  
+WHERE dh.EndDate IS NULL;  
+
+  
+SELECT DISTINCT Name AS DepartmentName  
+      ,PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY ph.Rate)   
+                            OVER (PARTITION BY Name) AS MedianCont  
+      ,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY ph.Rate)   
+                            OVER (PARTITION BY Name) AS MedianDisc  
+FROM HumanResources.Department AS d  
+INNER JOIN HumanResources.EmployeeDepartmentHistory AS dh   
+    ON dh.DepartmentID = d.DepartmentID  
+INNER JOIN HumanResources.EmployeePayHistory AS ph  
+    ON ph.BusinessEntityID = dh.BusinessEntityID  
+WHERE dh.EndDate IS NULL;
+
+SELECT Department, LastName, Rate,   
+       CUME_DIST () OVER (PARTITION BY Department ORDER BY Rate) AS CumeDist,   
+       PERCENT_RANK() OVER (PARTITION BY Department ORDER BY Rate ) AS PctRank  
+FROM HumanResources.vEmployeeDepartmentHistory AS edh  
+    INNER JOIN HumanResources.EmployeePayHistory AS e    
+    ON e.BusinessEntityID = edh.BusinessEntityID  
+WHERE Department IN (N'Information Services',N'Document Control')   
+ORDER BY Department, Rate DESC;  
