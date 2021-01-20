ee5f07dae3b8ff20db3dba1b1c3d437da4a373af
Ewout van der Wal <33218530+EwoutWal@users.noreply.github.com>
2021-01-20 17:46:08 +0100

    Update LexerAdaptor.cs

--- antlr/antlr4/LexerAdaptor.cs
+++ antlr/antlr4/LexerAdaptor.cs
@@ -26,7 +26,7 @@
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-namespace LexerAdaptor
+namespace Antlr2BGF.AntlrParser
 {
     using System;
     using System.IO;
@@ -36,10 +36,14 @@ namespace LexerAdaptor
 
     public abstract class LexerAdaptor : Lexer
     {
+        private static readonly int PREQUEL_CONSTRUCT = -10;
+
         // I copy a reference to the stream, so It can be used as a Char Stream, not as a IISStream
         readonly ICharStream stream;
+
         // Tokens are read only so I hack my way
         readonly FieldInfo tokenInput = typeof(CommonToken).GetField("_type", BindingFlags.NonPublic | BindingFlags.Instance);
+
         protected LexerAdaptor(ICharStream input)
              : base(input, Console.Out, Console.Error)
         {
@@ -65,7 +69,7 @@ namespace LexerAdaptor
          */
         private int CurrentRuleType { get; set; } = TokenConstants.InvalidType;
 
-        private bool InsideOptionsBlock { get; set; } = false;
+        private bool _insideOptionsBlock;
 
         protected void handleBeginArgument()
         {
@@ -85,59 +89,81 @@ namespace LexerAdaptor
             PopMode();
             if (ModeStack.Count > 0)
             {
-                CurrentRuleType = (ANTLRv4Lexer.ARGUMENT_CONTENT);
+                Type = ANTLRv4Lexer.ARGUMENT_CONTENT;
             }
         }
 
         protected void handleEndAction()
         {
-            PopMode();
-            if (ModeStack.Count > 0)
+            var oldMode = CurrentMode;
+            var newMode = PopMode();
+
+            if (ModeStack.Count > 0 && newMode == ANTLRv4Lexer.TargetLanguageAction && oldMode == newMode)
             {
-                CurrentRuleType = (ANTLRv4Lexer.ACTION_CONTENT);
+                Type = ANTLRv4Lexer.ACTION_CONTENT;
             }
         }
 
         protected void handleOptionsLBrace()
         {
-            if (InsideOptionsBlock)
+            if (_insideOptionsBlock)
             {
-                CurrentRuleType = ANTLRv4Lexer.BEGIN_ACTION;
+                Type = ANTLRv4Lexer.BEGIN_ACTION;
                 PushMode(ANTLRv4Lexer.TargetLanguageAction);
             }
             else
             {
-                CurrentRuleType = ANTLRv4Lexer.LBRACE;
-                InsideOptionsBlock = true;
+                Type = ANTLRv4Lexer.LBRACE;
+                _insideOptionsBlock = true;
             }
         }
-
 
         public override IToken NextToken()
         {
             var token = base.NextToken();
-            if (Type == ANTLRv4Lexer.ID)
+            if ((Type == ANTLRv4Lexer.OPTIONS || Type == ANTLRv4Lexer.TOKENS || Type == ANTLRv4Lexer.CHANNELS) && CurrentRuleType == TokenConstants.InvalidType)
             {
-                char firstChar = stream.GetText(Interval.Of(TokenStartCharIndex, TokenStartCharIndex))[0];
+                // enter prequel construct ending with an RBRACE
+                CurrentRuleType = PREQUEL_CONSTRUCT;
+            }
+            else if (Type == ANTLRv4Lexer.RBRACE && CurrentRuleType == PREQUEL_CONSTRUCT)
+            {
+                // exit prequel construct
+                CurrentRuleType = TokenConstants.InvalidType;
+            }
+            else if (Type == ANTLRv4Lexer.AT && CurrentRuleType == TokenConstants.InvalidType)
+            {
+                // enter action
+                CurrentRuleType = ANTLRv4Lexer.AT;
+            }
+            else if (Type == ANTLRv4Lexer.END_ACTION && CurrentRuleType == ANTLRv4Lexer.AT)
+            {
+                // exit action
+                CurrentRuleType = TokenConstants.InvalidType;
+            }
+            else if (Type == ANTLRv4Lexer.ID)
+            {
+                var firstChar = stream.GetText(Interval.Of(TokenStartCharIndex, TokenStartCharIndex))[0];
                 if (char.IsUpper(firstChar))
                 {
                     Type = ANTLRv4Lexer.TOKEN_REF;
                     tokenInput.SetValue(token, ANTLRv4Lexer.TOKEN_REF);
                 }
+
                 if (char.IsLower(firstChar))
                 {
-
                     Type = ANTLRv4Lexer.RULE_REF;
                     tokenInput.SetValue(token, ANTLRv4Lexer.RULE_REF);
                 }
 
                 if (CurrentRuleType == TokenConstants.InvalidType)
-                { // if outside of rule def
+                {
+                    // if outside of rule def
                     CurrentRuleType = Type; // set to inside lexer or parser rule
                 }
             }
             else if (Type == ANTLRv4Lexer.SEMI)
-            { // exit rule def
+            {
                 CurrentRuleType = TokenConstants.InvalidType;
             }
 
@@ -145,9 +171,5 @@ namespace LexerAdaptor
         }
 
         private bool InLexerRule => CurrentRuleType == ANTLRv4Lexer.TOKEN_REF;
-
-
-        private bool InParserRule => CurrentRuleType == ANTLRv4Lexer.RULE_REF;
-        
     }
 }
