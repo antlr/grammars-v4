63f206f9decb3e285fff9b08494a03dc0837cfd2
Ken Domino <kenneth.domino@domemtech.com>
2021-12-17 13:27:39 -0500

    Forgot to run locally "mvn clean test". Added path to CSharpParserBase.java, and removed println()'s.

--- csharp/Java/CSharpParserBase.java
+++ csharp/Java/CSharpParserBase.java
@@ -1,7 +1,7 @@
 import org.antlr.v4.runtime.*;
 import java.util.Stack;
 
-abstract class CSharpParserBase extends Parser
+public abstract class CSharpParserBase extends Parser
 {
     protected CSharpParserBase(TokenStream input)
     {
@@ -11,12 +11,9 @@ abstract class CSharpParserBase extends Parser
     protected boolean VerifyVar()
     {
 	CSharpParser.Local_variable_declarationContext local_var_decl = (CSharpParser.Local_variable_declarationContext)this._ctx;
-	System.out.println("Yo1 " + local_var_decl);
 	if (local_var_decl == null) return true;
-	var local_variable_type = local_var_decl.local_variable_type();
-	System.out.println("Yo2 " + local_variable_type);
+	CSharpParser.Local_variable_typeContext local_variable_type = local_var_decl.local_variable_type();
 	if (local_variable_type == null) return true;
-	System.out.println("Yo3 " + local_variable_type.getText());
 	if (local_variable_type.getText().equals("var")) return false;
 	return true;
     }
