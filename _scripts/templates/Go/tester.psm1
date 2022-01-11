cbb96f0d2c3892dfe32a1dbe1b95114f7c8756f6
Ken Domino <kenneth.domino@domemtech.com>
2022-01-11 12:33:52 -0500

    2nd attempt to turn off every damn build job except for Go, and only for abb/. 3rd attempt to redirect debug output to stdout. "pwsh" documentation for Out-Host: "The host displays the output at the command line." https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/out-host?view=powershell-7.2. Right, what IO stream number or name is "command line"?

--- _scripts/templates/Go/tester.psm1
+++ _scripts/templates/Go/tester.psm1
@@ -13,11 +13,13 @@ function Build-Grammar {
     # Attempt 1. Does not work--captured.
     # Get-Host | Select-Object Version
     # Attempt 2.
-    Get-Host | Select-Object Version | Out-Host
+    # Get-Host | Select-Object Version | Out-Host
+    # Attempt 3.
+    Get-Host | Select-Object Version | Out-Host | Write-Host
     # Output environmental variables.
-    dir env: | Out-Host
+    dir env: | Out-Host | Write-Host
     # Output go version
-    go version | Out-Host
+    go version | Out-Host | Write-Host
     #
     $env:GO111MODULE = "on"
     $g = go get github.com/antlr/antlr4/runtime/Go/antlr@4.9.3
