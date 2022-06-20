09e231a95b31b4a9f9b22bc32864e03ce2aed71f
harveyyue <yw_yuewei@126.com>
2022-06-20 23:50:40 +0800

    DBZ-5271 Support set role statement for mysql8

--- sql/mysql/Positive-Technologies/examples/ddl_create.sql
+++ sql/mysql/Positive-Technologies/examples/ddl_create.sql
@@ -473,6 +473,13 @@ BEGIN
 END; -- //-- delimiter ;
 #end
 #begin
+-- Create Role
+create role 'RL_COMPLIANCE_NSA';
+create role if not exists 'RL_COMPLIANCE_NSA';
+CREATE ROLE 'admin', 'developer';
+CREATE ROLE 'webapp'@'localhost';
+#end
+#begin
 CREATE VIEW view_with_cte1 AS
 WITH cte1 AS
 (
