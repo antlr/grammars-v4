b4c8c3720658eee95c637efa886cbb1886c736ef
Ken Domino <kenneth.domino@domemtech.com>
2022-07-13 07:58:10 -0400

    Fix transformGrammar.py to work on all files in current directory. Add step to call transform in CI build. Add javascript/Python3 to build.

--- javascript/javascript/Python3/transformGrammar.py
+++ javascript/javascript/Python3/transformGrammar.py
@@ -1,11 +1,11 @@
 import sys, os, re, shutil
 
-if __name__ == "__main__":
-    print("Python utility to transform the JavaScript Parser and Lexer grammars to work with a python output")
-    if len(sys.argv) == 1:
-        print(f"""Please run this script as {sys.argv[0]} <grammar>.g4""")
-        sys.exit(0)
-    file_path = sys.argv[1]
+def main(argv):
+    fix("JavaScriptLexer.g4")
+    fix("JavaScriptParser.g4")
+
+def fix(file_path):
+    print("Altering " + file_path)
     if not os.path.exists(file_path):
         print(f"Could not find file: {file_path}")
         sys.exit(1)
@@ -34,5 +34,9 @@ if __name__ == "__main__":
             output_file.write(x)
             output_file.flush()
 
+    print("Writing ...")
     input_file.close()
     output_file.close()
+
+if __name__ == '__main__':
+    main(sys.argv)
