d05132e86dd3136a44cbcf98b89c79b775d9e631
Robert Einhorn <robert.einhorn.hu@gmail.com>
2022-09-07 14:54:12 +0200

    python-3.8.12
    added python-3.8.12 directory
    removed python3-without-actions directory
    edited skip files

---
+++ python/python-3.8.12/indentation_tests/test_error_inconsistent_dedent.py
@@ -0,0 +1,6 @@
+# REQUIREMENT:
+# - error message: "line 6:         unindent does not match any outer indentation level"
+
+if True:
+    i = 0
+  j = 0  # inconsistent dedent
---
+++ python/python-3.8.12/indentation_tests/test_hidden_leading_NEWLINEs.py
@@ -0,0 +1,4 @@
+# REQUIREMENTS:
+# - hidden NEWLINE tokens before the first statement
+# - no error message
+i = 1
