d0623e21b2f7ca4a575d2fb9292234323114f790
Ken Domino <kenneth.domino@domemtech.com>
2022-09-08 00:10:10 -0400

    Update Go template, require version 1.18 of Go.

--- _scripts/templates/Go/tester.psm1
+++ _scripts/templates/Go/tester.psm1
@@ -17,7 +17,7 @@ function Build-Grammar {
     #go version | Write-Host
     $env:GO111MODULE = "on"
     For ($i=0; $i -le 5; $i++) {
-        $g = go get github.com/antlr/antlr4/runtime/Go/antlr@4.11
+        $g = go get github.com/antlr/antlr4/runtime/Go/antlr/v4
         if($LASTEXITCODE -eq 0){
             Break
         }
