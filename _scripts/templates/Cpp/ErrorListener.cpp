a24c4a433a9482c62b30615bcbe52d92f0811bf9
Ross Patterson <ross.patterson@gmail.com>
2022-10-03 15:22:22 -0400

    Fix Cpp target error listener.  C++ makes the simplest things weird.

--- _scripts/templates/Cpp/ErrorListener.cpp
+++ _scripts/templates/Cpp/ErrorListener.cpp
@@ -5,5 +5,5 @@
 void ErrorListener::syntaxError(antlr4::Recognizer* recognizer, antlr4::Token* offendingSymbol, size_t line, size_t col, const std::string& msg, std::exception_ptr e)
 {
     had_error = true;
-    std::cout \<\< "line " + line + ":" + charPositionInLine + " " + msg \<\< std::endl;
+    std::cout \<\< "line " \<\< line \<\< ":" \<\< col \<\< " " \<\< msg \<\< std::endl;
 }
