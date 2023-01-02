b21e12601d5d0d69152eb67bf2989dc37cce789a
mattmcd <matt@matt-mcdonnell.com>
2023-01-02 07:39:01 +0000

    Split select tests into separate files to debug PHP build timeout error.  Comment out data type as column names test as a this seems a likely cause

--- sql/snowflake/examples/select.sql
+++ sql/snowflake/examples/select.sql
@@ -1,13 +1,3 @@
-select
-  userhash
-  , case when (a = 1) then 1 else 0 end as v1
-  , iff(a = 1, 1, 0) as v2
-from t
-;
-
 -- Table with types and functions as column names
-select user, date, iff, current_timestamp, cluster, type, region, function from edge;
-
-
--- having
-select department_id from employees group by department_id having count(*) < 10;
+-- Causes timeout on PHP build test?
+-- select user, date, iff, current_timestamp, cluster, type, region, function from edge;
