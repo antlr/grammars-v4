835158ee26d5fab32d303d3841b44cf1e6fefe04
Ken Domino <kenneth.domino@domemtech.com>
2023-01-15 13:23:47 -0500

    Add in code to get dependencies.

--- _scripts/templates/PHP/clean.sh
+++ _scripts/templates/PHP/clean.sh
@@ -1,5 +1,12 @@
 # Generated from trgen <version>
+rm -f *.interp
+files=()
+<tool_grammar_tuples:{x |
+files+=( `java -jar "<antlr_tool_path>" -depend -encoding <antlr_encoding> -Dlanguage=PHP <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileName> | awk '{print $1\}' | grep -v ':'` )
+} >
+for i in ${files[*]}
+do
+    rm -f $i
+done
 rm -rf vendor composer.lock
-rm -f *.tokens *.interp
-rm -f <tool_grammar_tuples:{x|<x.GeneratedFileName> }>
 exit 0
