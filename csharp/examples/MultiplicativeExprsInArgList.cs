c25380de2b72466e3c782f61f193d8f1c91505f0
MEPalma <marcoepalma@tutanota.com>
2023-01-18 16:26:24 +0100

    Example for changes introduced in PT3005

--- csharp/examples/MultiplicativeExprsInArgList.cs
+++ csharp/examples/MultiplicativeExprsInArgList.cs
@@ -1,6 +1,6 @@
 public class ClassName {
     Constructor MethodName(Object a, String b, int c) {
-        val avgSum = Math.Sqrt(Math.Pow(x[i] - x[j], 2) + Math.Pow(y[i] - y[j], 2));
+        var avgSum = Math.Sqrt(Math.Pow(x[i] - x[j], 2) + Math.Pow(y[i] - y[j], 2));
         return new Constructor(field, a.b, a.b + c.d, e.f.g + h);
     }
 }
