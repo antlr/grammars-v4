7050811dedc43785043c55c6e91549491a3596aa
M Lorek <mlorek@users.noreply.github.com>
2023-02-18 01:41:28 +0000

    comment on view, materialized view with grace period (#3094)

--- sql/trino/examples/create_materialized_view.sql
+++ sql/trino/examples/create_materialized_view.sql
@@ -3,3 +3,4 @@ CREATE OR REPLACE MATERIALIZED VIEW catalog.schema.matview COMMENT 'A simple mat
 CREATE OR REPLACE MATERIALIZED VIEW catalog.schema.matview COMMENT 'A simple materialized view' AS SELECT * FROM catalog2.schema2.tab;
 CREATE OR REPLACE MATERIALIZED VIEW catalog.schema.matview COMMENT 'A simple materialized view'WITH (partitioned_by = ARRAY ['dateint']) AS SELECT * FROM catalog2.schema2.tab;
 CREATE OR REPLACE MATERIALIZED VIEW catalog.schema.matview COMMENT 'A partitioned materialized view' WITH (partitioned_by = ARRAY ['dateint']) AS WITH a (t, u) AS (SELECT * FROM x), b AS (SELECT * FROM a) TABLE b;
+CREATE MATERIALIZED VIEW a GRACE PERIOD INTERVAL '2' DAY AS SELECT * FROM t;
