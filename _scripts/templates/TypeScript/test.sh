88d72e632b4f7175ce60f978333a6f4514bff065
Ken Domino <kenneth.domino@domemtech.com>
2023-03-04 14:18:08 -0500

    Fix for 3173: TypeScript testing does not work on Linux and MacOS (#3175)
    * Fix for https://github.com/antlr/grammars-v4/issues/3173
    
    * Do not try to run bash from pwsh, it does not work.
    
    * Reorder target list (keep alphabetically, please) in order to test TypeScript target.

--- _scripts/templates/TypeScript/test.sh
+++ _scripts/templates/TypeScript/test.sh
@@ -32,17 +32,7 @@ do
 done
 
 # Parse all input files.
-# ts-node is a bash script, so duplicate that code and call node via trwdog.
-tsnode=`which ts-node`
-basedir=$(dirname "$(echo "$tsnode" | sed -e 's,\\\\,/,g')")
-case `uname` in
-    *CYGWIN*|*MINGW*|*MSYS*) basedir=`cygpath -w "$basedir"`;;
-esac
-if [ -x "$basedir/node" ]; then
-  echo "${files[*]}" | trwdog "$basedir/node" "$basedir/node_modules/ts-node/dist/bin.js" Test.js -q -x -tee -tree 2>&1 > parse.txt
-else 
-  echo "${files[*]}" | trwdog node "$basedir/node_modules/ts-node/dist/bin.js" Test.js -q -x -tee -tree 2>&1 > parse.txt
-fi
+echo "${files[*]}" | trwdog bash -c "ts-node Test.js -q -x -tee -tree" 2>&1 > parse.txt
 status="$?"
 
 # trwdog returns 255 if it cannot spawn the process. This could happen
