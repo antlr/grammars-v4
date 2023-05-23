26cbb6e8c81d42258394bd2b01cee2a91c9426a1
Ken Domino <kenneth.domino@domemtech.com>
2023-05-23 08:26:08 -0400

    Fixes for #3468. JavaScript.
    Fix the JavaScript build scripts, including the package.json version for Antlr4 runtime.

--- _scripts/templates/JavaScript/build.ps1
+++ _scripts/templates/JavaScript/build.ps1
@@ -3,8 +3,14 @@ if (Test-Path -Path transformGrammar.py -PathType Leaf) {
     $(& python3 transformGrammar.py ) 2>&1 | Write-Host
 }
 
+# Because there is no integrated build script for Dart targets, we need
+# to manually look at the version in package.json and extract the
+# version number. We can then use this with antlr4 to generate the
+# parser and lexer.
+$version = Select-String -Path "package.json" -Pattern "antlr4" | ForEach-Object {$_.Line.Split(" ")[5]} | ForEach-Object {$_.Trim('"')} | ForEach-Object {$_.Trim(',')}
+
 <tool_grammar_tuples:{x |
-$(& java -jar <antlr_tool_path> <x.GrammarFileName> -encoding <antlr_encoding> -Dlanguage=JavaScript <x.AntlrArgs> <antlr_tool_args:{y | <y> } > ; $compile_exit_code = $LASTEXITCODE) | Write-Host
+$(& antlr4 -v $version <x.GrammarFileName> -encoding <antlr_encoding> -Dlanguage=JavaScript <x.AntlrArgs> <antlr_tool_args:{y | <y> } > ; $compile_exit_code = $LASTEXITCODE) | Write-Host
 if($compile_exit_code -ne 0){
     exit $compile_exit_code
 \}
--- _scripts/templates/JavaScript/build.sh
+++ _scripts/templates/JavaScript/build.sh
@@ -3,8 +3,14 @@ set -e
 
 if [ -f transformGrammar.py ]; then python3 transformGrammar.py ; fi
 
+# Because there is no integrated build script for Dart targets, we need
+# to manually look at the version in package.json and extract the
+# version number. We can then use this with antlr4 to generate the
+# parser and lexer.
+version=`grep antlr4 package.json | awk '{print $2}' | tr -d '"' | tr -d ','`
+
 <tool_grammar_tuples:{x |
-java -jar "<antlr_tool_path>" -encoding <antlr_encoding> -Dlanguage=JavaScript <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileName>
+antlr4 -v $version -encoding <antlr_encoding> -Dlanguage=JavaScript <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileName>
 } >
 
 npm install
