ddf6d2a378b977a53e386fe4222644f074644d4c
Ken Domino <kenneth.domino@domemtech.com>
2023-05-31 07:55:33 -0400

    Fixes for #3480
    This fixes #4580, where .csproj files were not recognized and updated by Dependabot. In this implementation, file Other.csproj is added, along with Other.csproj.meta. The Other.csproj file is in MSBuild format, not StringTemplate format, as there is a .meta file that turns off StringTemplate processing, and just copies the file to the generated driver directory. The Test.csproj file now contains an "Import" statement, where it includes Other.csproj. Because there are two csproj files, the rest of the changes clear up the ambiguity with dotnet builds.

--- _scripts/templates/CSharp/build.ps1
+++ _scripts/templates/CSharp/build.ps1
@@ -3,5 +3,5 @@ if (Test-Path -Path transformGrammar.py -PathType Leaf) {
     $(& python3 transformGrammar.py ) 2>&1 | Write-Host
 }
 
-$(& dotnet build; $compile_exit_code = $LASTEXITCODE) | Write-Host
+$(& dotnet build Test.csproj; $compile_exit_code = $LASTEXITCODE) | Write-Host
 exit $compile_exit_code
