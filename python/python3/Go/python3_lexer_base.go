29124fe7e39152e493a6c9ebf541d4a744c56b09
Ken Domino <kenneth.domino@domemtech.com>
2023-07-02 21:14:34 -0400

    Fixes for Go target of python3 grammar.
    This port works but only if the Go runtime is fixed, and "Virt" is assigned in the driver. See https://github.com/antlr/antlr4/pull/4343 https://github.com/antlr/antlr4/issues/4342

--- python/python3/Go/python3_lexer_base.go
+++ python/python3/Go/python3_lexer_base.go
@@ -1,52 +1,56 @@
 package parser
 
 import (
-	"github.com/antlr4-go/antlr/v4"
 	"regexp"
+
+	"github.com/antlr4-go/antlr/v4"
 )
 
 type Python3LexerBase struct {
-    *antlr.BaseLexer
-    tokens []antlr.Token
-    indents []int
-    Opened int
-    lastToken antlr.Token
+	*antlr.BaseLexer
+	tokens    []antlr.Token
+	indents   []int
+	Opened    int
+	lastToken antlr.Token
 }
 
+func (l *Python3LexerBase) Emit() antlr.Token {
+	t := l.BaseLexer.Emit()
+	return t
+}
 
+func (l *Python3LexerBase) EmitToken(t antlr.Token) {
+	l.BaseLexer.EmitToken(t)
+	l.tokens = append(l.tokens, t)
+}
 
 func (l *Python3LexerBase) MakeCommonToken(ttype int, text string) antlr.Token {
-    stop := l.TokenStartCharIndex - 1
-    start := stop
-    if len(text) != 0 {
-        start = stop - len(text) + 1
-    }
-    ctf := l.GetTokenFactory()
-        t := ctf.Create(
-            l.GetTokenSourceCharStreamPair(),
-            ttype,
-            text,
-            antlr.TokenDefaultChannel,
-            start,
-            l.TokenStartCharIndex - 1,
-            l.TokenStartLine,
-            l.TokenStartColumn)
-        return t
-}
-
-func (l *Python3LexerBase) EmitToken(token antlr.Token) {
-    l.BaseLexer.EmitToken(token)
-    l.tokens = append(l.tokens, token)
+	stop := l.TokenStartCharIndex - 1
+	start := stop
+	if len(text) != 0 {
+		start = stop - len(text) + 1
+	}
+	ctf := l.GetTokenFactory()
+	t := ctf.Create(
+		l.GetTokenSourceCharStreamPair(),
+		ttype,
+		text,
+		antlr.TokenDefaultChannel,
+		start,
+		l.TokenStartCharIndex-1,
+		l.TokenStartLine,
+		l.TokenStartColumn)
+	return t
 }
 
 func (l *Python3LexerBase) CreateDedent() antlr.Token {
-    dedent := l.MakeCommonToken(Python3ParserDEDENT, "")
-    //dedent.Line = LastToken.GetLine()
-    return dedent
+	dedent := l.MakeCommonToken(Python3ParserDEDENT, "")
+	//dedent.Line = LastToken.GetLine()
+	return dedent
 }
 
 func (l *Python3LexerBase) NextToken() antlr.Token {
-	if (l.GetInputStream().LA(1) == antlr.TokenEOF && len(l.indents) != 0) {
+	if l.GetInputStream().LA(1) == antlr.TokenEOF && len(l.indents) != 0 {
 		var filtered []antlr.Token
 		for _, value := range l.tokens {
 			if value.GetTokenType() != antlr.TokenEOF {
@@ -63,10 +67,10 @@ func (l *Python3LexerBase) NextToken() antlr.Token {
 	}
 
 	next := l.BaseLexer.NextToken()
-	if (next.GetChannel() == antlr.TokenDefaultChannel) {
+	if next.GetChannel() == antlr.TokenDefaultChannel {
 		l.lastToken = next
 	}
-	if (len(l.tokens) == 0) {
+	if len(l.tokens) == 0 {
 		return next
 	} else {
 		x := l.tokens[0]
@@ -78,7 +82,7 @@ func (l *Python3LexerBase) NextToken() antlr.Token {
 func (l *Python3LexerBase) GetIndentationCount(spaces string) int {
 	count := 0
 	for _, ch := range spaces {
-		if (ch == '\t') {
+		if ch == '\t' {
 			count += 8 - (count % 8)
 		} else {
 			count += 1
@@ -88,15 +92,15 @@ func (l *Python3LexerBase) GetIndentationCount(spaces string) int {
 }
 
 func (l *Python3LexerBase) atStartOfInput() bool {
-    return l.TokenStartColumn == 0 && l.TokenStartLine == 1
+	return l.TokenStartColumn == 0 && l.TokenStartLine == 1
 }
 
 func (l *Python3LexerBase) openBrace() {
-    l.Opened++
+	l.Opened++
 }
 
 func (l *Python3LexerBase) closeBrace() {
-    l.Opened--
+	l.Opened--
 }
 
 func (l *Python3LexerBase) onNewLine() {
@@ -104,38 +108,37 @@ func (l *Python3LexerBase) onNewLine() {
 	newLine := newLineRegex.ReplaceAllString(l.GetText(), "")
 	spacesRegex := regexp.MustCompile(`[\r\n\f]+`)
 	spaces := spacesRegex.ReplaceAllString(l.GetText(), "")
-    next := l.GetInputStream().LA(1)
-    nextnext := l.GetInputStream().LA(2)
-    if (l.Opened > 0 || (nextnext != -1 && (next == '\r' || next == '\n' || next == '\f' || next == '#'))) {
-        l.Skip()
-    } else {
-        l.EmitToken(l.MakeCommonToken(Python3LexerNEWLINE, newLine))
-        indent := l.GetIndentationCount(spaces)
-        previous := 0
+	next := l.GetInputStream().LA(1)
+	nextnext := l.GetInputStream().LA(2)
+	if l.Opened > 0 || (nextnext != -1 && (next == '\r' || next == '\n' || next == '\f' || next == '#')) {
+		l.Skip()
+	} else {
+		l.EmitToken(l.MakeCommonToken(Python3LexerNEWLINE, newLine))
+		indent := l.GetIndentationCount(spaces)
+		previous := 0
 		if len(l.indents) == 0 {
 			previous = 0
-        } else {
+		} else {
 			previous = l.indents[len(l.indents)-1]
 		}
-        if (indent == previous) {
-            l.Skip()
-        } else if (indent > previous) {
-            l.indents = append(l.indents, indent)
-            l.EmitToken(l.MakeCommonToken(Python3ParserINDENT, spaces))
-        } else {
-            for len(l.indents) != 0 && l.indents[len(l.indents)-1] > indent {
-                l.EmitToken(l.CreateDedent())
+		if indent == previous {
+			l.Skip()
+		} else if indent > previous {
+			l.indents = append(l.indents, indent)
+			l.EmitToken(l.MakeCommonToken(Python3ParserINDENT, spaces))
+		} else {
+			for len(l.indents) != 0 && l.indents[len(l.indents)-1] > indent {
+				l.EmitToken(l.CreateDedent())
 				l.indents = l.indents[:len(l.indents)-1]
-            }
-        }
-    }
+			}
+		}
+	}
 }
 
-
 func (l *Python3LexerBase) Reset() {
-    l.tokens = make([]antlr.Token, 0)
-    l.indents = make([]int, 0)
-    l.Opened = 0
-    //l.lastToken = null
-    l.BaseLexer.Reset()
+	l.tokens = make([]antlr.Token, 0)
+	l.indents = make([]int, 0)
+	l.Opened = 0
+	//l.lastToken = null
+	l.BaseLexer.Reset()
 }
