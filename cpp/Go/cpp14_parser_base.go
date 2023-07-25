e46b5b443b692f4af9ee766d2e603efb607d6ce2
Ken Domino <kenneth.domino@domemtech.com>
2023-07-25 07:35:18 -0400

    Fix Go base class.

--- cpp/Go/cpp14_parser_base.go
+++ cpp/Go/cpp14_parser_base.go
@@ -2,8 +2,7 @@ package parser
 
 import (
 	"github.com/antlr4-go/antlr/v4"
-//	"reflect"
-//	"fmt"
+	"reflect"
 )
 
 // ParserBase implementation.
@@ -14,24 +13,19 @@ type CPP14ParserBase struct {
 
 // Returns true if the current Token is a closing bracket (")" or "}")
 func (p *CPP14ParserBase) IsPureSpecifierAllowed() bool {
-//    fmt.Println("YO!")
     x := p.GetParserRuleContext();
-//    fmt.Println("YO 1", x)
     y := x.GetChild(0)
-//    fmt.Println("YO 2", y)
     c := y.GetChild(0)
-//    fmt.Println("YO 3 ", c)
     c2 := c.GetChild(0)
-//    fmt.Println("YO 4 ", c2)
     if c2.GetChildCount() <= 1 {
-//	fmt.Println("return false")
 	return false
     }
-//    c3 := c2.GetChild(1)
-//    if c3 == nil {
-//        return false
-//    }
-//    return reflect.TypeOf(c3).Elem() == reflect.TypeOf(ParametersAndQualifiersContext{}).Elem()
-//    fmt.Println("return true")
-    return true
+    c3 := c2.GetChild(1)
+    if c3 == nil {
+        return false
+    }
+    ce1 := reflect.TypeOf(c3).Elem()
+    ce2 := reflect.TypeOf(ParametersAndQualifiersContext{})
+    ce := ce1 == ce2
+    return ce
 }
