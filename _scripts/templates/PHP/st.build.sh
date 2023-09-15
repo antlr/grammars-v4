9b7ae73b3e640809e091a475bbc700f61a7369c5
Ken Domino <kenneth.domino@domemtech.com>
2023-09-15 15:47:33 -0400

    Fix for #3125 -- Add Antlr4 PHP target testing (#3699)
    * Fix for #3125
    
    Update build script to extract runtime version from composer.json, then find the tool version from online sources. Add in PHP testing.
    
    * Update Pwsh build.ps1 script.
    
    * Remove build-pwsh PHP -- it is tested with build-bash.
    
    * Update desc.xml to remove PHP testing for two grammars.
    
    * Fix compiler error for TypeScript target.
    
    Fixed to use corrected API. https://github.com/antlr/antlr4/pull/4319

--- _scripts/templates/PHP/st.build.sh
+++ _scripts/templates/PHP/st.build.sh
@@ -7,10 +7,31 @@ if [ -f transformGrammar.py ]; then python3 transformGrammar.py ; fi
 # to manually look at the version in pubspec.yaml and extract the
 # version number. We can then use this with antlr4 to generate the
 # parser and lexer.
-version=4.13.0; runtime_version=0.9.0
+
+# This table lookup is caused because the PHP does not
+# use the same version number as the tool.
+
+# Find runtime version in composer.json file.
+runtime_version=`grep antlr4 composer.json | awk '{print $2}' | tr -d '\r' | tr -d '\n' | tr -d ',' | tr -d '"'`
+
+# Get from online sources the commit version number that corresponds to the runtime version.
+rm -rf antlr4-php-runtime
+wget "https://packagist.org/packages/antlr/antlr4-php-runtime#$runtime_version"
+commit_version=`grep BSD-3-Clause antlr4-php-runtime | awk '{print $NF}' | sed 's/\<.*//'`
+
+# Checkout the sources from the php runtime repo.
+rm -rf antlr4-php-runtime
+git clone https://github.com/antlr/antlr-php-runtime
+cd antlr-php-runtime
+git checkout $commit_version
+
+# Extract the tool version this damn runtime version corresponds to.
+tool_version=`grep 'public const VERSION' src/RuntimeMetaData.php | awk '{print $NF}' | sed "s/'//g" | sed 's/;//'`
+cd ..
+rm -rf antlr4-php-runtime
 
 <tool_grammar_tuples:{x |
-antlr4 -v $version -encoding <antlr_encoding> -Dlanguage=PHP <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileName>
+antlr4 -v $tool_version -encoding <antlr_encoding> -Dlanguage=PHP <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileName>
 }>
 
 composer install
