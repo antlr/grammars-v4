843863073d4fdfc0039e5f942c0d33b5e18b59e4
Ken Domino <kenneth.domino@domemtech.com>
2023-10-10 15:52:00 -0400

    Fix.

--- lua/JavaScript/LuaLexerBase.js
+++ lua/JavaScript/LuaLexerBase.js
@@ -1,15 +1,8 @@
-import antlr4 from 'antlr4';
-import LuaLexer from './LuaLexer.js';
-
-export default class LuaLexerBase extends antlr4.Lexer
-{
-    constructor(input)
-    {
+import { Lexer } from "antlr4";
+export default class BisonLexerBase extends Lexer {
+    constructor(input) {
         super(input);
-        this.start_line = 0;
-        this.start_col = 0;
     }
-
     HandleComment() {
         this.start_line = this.line;
         this.start_col = this.column - 2;
@@ -25,17 +18,16 @@ export default class LuaLexerBase extends antlr4.Lexer
             cs.consume();
         }
     }
-
     read_long_string(cs, sep) {
         let done = false;
         cs.consume();
-        for (; ;) {
+        for (;;) {
             let c = cs.LA(1);
             switch (c) {
                 case -1:
                     done = true;
-                    let listener = this.getErrorListenerDispatch();
-                    listener.syntaxError(this, null, this.start_line, this.start_col, "unfinished long comment", null);
+                    //                    let listener = this.getErrorListenerDispatch();
+                    //                    listener.syntaxError(this, null, this.start_line, this.start_col, "unfinished long comment", null);
                     break;
                 case 93: /* ']' */
                     if (this.skip_sep(cs) === sep) {
@@ -51,10 +43,10 @@ export default class LuaLexerBase extends antlr4.Lexer
                     cs.consume();
                     break;
             }
-            if (done) break;
+            if (done)
+                break;
         }
     }
-
     skip_sep(cs) {
         let count = 0;
         let s = cs.LA(1);
@@ -63,12 +55,14 @@ export default class LuaLexerBase extends antlr4.Lexer
             cs.consume();
             count++;
         }
-        if (cs.LA(1) === s) count += 2;
-        else if (count === 0) count = 1;
-        else count = 0;
+        if (cs.LA(1) === s)
+            count += 2;
+        else if (count === 0)
+            count = 1;
+        else
+            count = 0;
         return count;
     }
-
     IsLine1Col0() {
         let cs = this._input;
         return cs.index === 1;
