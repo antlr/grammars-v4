de1f4e0fb305545fce59fc95aeeaab5298b68946
Ken Domino <kenneth.domino@domemtech.com>
2023-10-10 07:18:21 -0400

    Fix CSharp target.

--- lua/CSharp/LuaLexerBase.cs
+++ lua/CSharp/LuaLexerBase.cs
@@ -55,14 +55,24 @@ public abstract class LuaLexerBase : Lexer
                     done = true;
                     IAntlrErrorListener<int> listener = ErrorListenerDispatch;
                     listener.SyntaxError(ErrorOutput, this, 0, this.start_line, this.start_col, "unfinished long comment", null);
+                    done = true;
                     break;
                 case ']':
-                    if (skip_sep(cs) == sep) done = true;
+                    if (skip_sep(cs) == sep)
+                    {
+                        cs.Consume();
+                        done = true;
+                    }
+                    break;
+                default:
+                    if (cs.LA(1) == -1)
+                    {
+                        done = true;
+                        break;
+                    }
+                    cs.Consume();
                     break;
             }
-
-            if (cs.LA(1) == -1) break;
-            cs.Consume();
             if (done) break;
         }
     }
