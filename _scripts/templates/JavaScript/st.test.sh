257125e5c61defe2660be5b47817c6cafb731b7b
Ken Domino <kenneth.domino@domemtech.com>
2023-11-16 17:06:34 -0500

    Updated templates for Bash testing.

--- _scripts/templates/JavaScript/st.test.sh
+++ _scripts/templates/JavaScript/st.test.sh
@@ -12,7 +12,12 @@ IFS=$(echo -en "\n\b")
 # Get a list of test files from the test directory. Do not include any
 # .errors or .tree files. Pay close attention to remove only file names
 # that end with the suffix .errors or .tree.
-files2=`dotnet trglob -- '../<example_files_unix>' | grep -v '.errors$' | grep -v '.tree$'`
+if [ "$global" == "" ]
+then
+    files2=`dotnet trglob -- '../<example_files_unix>' | grep -v '.errors$' | grep -v '.tree$'`
+else
+    files2=`trglob '../<example_files_unix>' | grep -v '.errors$' | grep -v '.tree$'`
+fi
 
 files=()
 for f in $files2
