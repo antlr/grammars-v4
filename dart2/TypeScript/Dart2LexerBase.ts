d3f00c1a626b04f971f7b88cbffb978c8a83aaa2
Ken Domino <kenneth.domino@domemtech.com>
2023-11-18 05:31:59 -0500

    Fix indentation.

--- dart2/TypeScript/Dart2LexerBase.ts
+++ dart2/TypeScript/Dart2LexerBase.ts
@@ -1,9 +1,7 @@
 import { Lexer } from "antlr4";
 
 export default abstract class Dart2LexerBase extends Lexer {
-
-  CheckNotOpenBrace(): boolean {
-    return this._input.LA(1) !== "{".codePointAt(0);
-  }
-
+    CheckNotOpenBrace(): boolean {
+        return this._input.LA(1) !== "{".codePointAt(0);
+    }
 }
