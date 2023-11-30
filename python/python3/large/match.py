0ee5a1adca51e2f18d49edd27bf4cd3e3a89171d
Ken Domino <kenneth.domino@domemtech.com>
2023-11-30 07:41:37 -0500

    Remove python3/

--- python/python3/large/match.py
+++
@@ -1,8 +0,0 @@
-command = 'Hello, World!'
-match command:
-    case 'Hello, World!':
-        print('Hello to you too!')
-    case 'Goodbye, World!':
-        print('See you later')
-    case other:
-        print('No match found')
