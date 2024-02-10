bda98fc7a772b22d60dfb2248b8e376ef4aa8440
Dmitry <dmitry.mm@icloud.com>
2024-02-10 21:19:54 +0300

    [PlSql] remove `streaming_clause` from `create_function_body` - confused create function rule with function body rule

--- sql/plsql/examples/create_function05.sql
+++ sql/plsql/examples/create_function05.sql
@@ -1,24 +1,10 @@
-CREATE FUNCTION fun_streaming(p ref_cursor_type) RETURN tab_rec_type PIPELINED
-  CLUSTER p BY Region
-  PARALLEL_ENABLE(PARTITION p BY Region) IS
-  ret_rec rec_type;
-  cnt number;
-  sum number;
-BEGIN
-  FOR rec IN p LOOP
-    IF (first rec in the group) THEN
-        cnt := 1;
-        sum := rec.Sales;
-    ELSIF (last rec in the group) THEN
-      IF (cnt <> 0) THEN
-        ret_rec.Region := rec.Region;
-          ret_rec.AvgSales := sum/cnt;
-          PIPE ROW(ret_rec);
-        END IF;
-    ELSE
-       cnt := cnt + 1;
-      sum := sum + rec.Sales;
-    END IF;
-  END LOOP;
-  RETURN;
-END;
+CREATE FUNCTION get_val
+   ( x_val IN NUMBER,
+    y_val IN NUMBER,
+    image IN LONG RAW )
+   RETURN BINARY_INTEGER AS LANGUAGE C
+      NAME "c_get_val"
+      LIBRARY c_utils
+      PARAMETERS ( c_x_val int
+                 , c_y_val int
+                 , c_image BY REFERENCE byte);
