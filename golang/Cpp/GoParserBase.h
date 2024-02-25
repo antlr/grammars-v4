bcc9ec1b35e4ede2f90abdb8713a56315a634a92
neon12345 <784389+neon12345@users.noreply.github.com>
2024-02-25 21:09:12 +0100

    add support for Cpp target to golang (#3969)
    * Add Cpp support files for golang
    
    * Add Cpp parser include to GoParser.g4
    
    * Enable Cpp in desc.xml

---
+++ golang/Cpp/GoParserBase.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "antlr4-runtime.h"
+
+/**
+ * All parser methods that used in grammar (p, prev, notLineTerminator, etc.)
+ * should start with lower case char similar to parser rules.
+ */
+class GoParserBase : public antlr4::Parser {
+public:
+    GoParserBase(antlr4::TokenStream* input) : Parser(input) {
+    }
+
+    virtual ~GoParserBase() {}
+
+protected:
+    /**
+     * Returns true if the current Token is a closing bracket (")" or "}")
+     */
+    bool closingBracket();
+};
