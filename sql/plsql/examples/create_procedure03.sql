d74faddb36d51cbcc68ae3adeb9592fce45ef774
Laurent Bovet <laurent.bovet@windmaster.ch>
2024-03-11 11:47:14 +0100

    [PlSql] Fix separator of top-level routines preventing subsequent statements (#4005)

--- sql/plsql/examples/create_procedure03.sql
+++ sql/plsql/examples/create_procedure03.sql
@@ -1,11 +1,9 @@
-/*
-    CREATE OR REPLACE PROCEDURE prc_hw
-      IS
-    BEGIN
-        DBMS_OUTPUT.PUT_LINE('Hello World!');
-    END;
-    /
-*/
+CREATE OR REPLACE PROCEDURE prc_hw
+  IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Hello World!');
+END;
+/
 
 CREATE OR REPLACE PROCEDURE prc_pragma_inline
   IS
