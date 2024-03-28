140ae024c712e2b7334670273d280d2668fb601d
Ken Domino <kenneth.domino@domemtech.com>
2024-03-28 05:39:33 -0400

    Update test list computation.

--- _scripts/perf-changed.sh
+++ _scripts/perf-changed.sh
@@ -19,15 +19,6 @@ then
     exit 1
 fi
 
-function getopts-extra () {
-    OPTARG=()
-    declare i=0
-    # if the next argument is not an option, then append it to array OPTARG
-    while [[ ${OPTIND} -le $# && ${!OPTIND:0:1} != '-' ]]; do
-        OPTARG[i]=${!OPTIND}
-        let i++ OPTIND++
-    done
-}
 cwd=`pwd`
 
 while getopts 'a:b:' opt; do
@@ -63,6 +54,13 @@ echo PRS = ${prs[@]}
 echo COM = ${com[@]}
 echo '#PRS' = ${#prs[@]}
 
+# Clean up.
+for ((i=0; i<${#prs[@]}; i++))
+do
+    rm -rf "$cwd/${prs[$i]}"
+done
+rm -rf `find . -name 'Generated-*'`
+
 # The PR that is more recent is the first in the list.
 # Get grammars changed for current PR. This will focus exactly on what to
 # test. Note, we only consider .g4 changes, no examples, no duplicates.
@@ -79,14 +77,14 @@ do
     pushd $g > /dev/null 2> /dev/null
     while true
     do
-	    if [ -f `pwd`/desc.xml ]
-	    then
-	        break
-	    elif [ `pwd` == "$prefix" ]
-	    then
-	        break
-	    fi
-	    cd ..
+        if [ -f `pwd`/desc.xml ]
+        then
+            break
+        elif [ `pwd` == "$prefix" ]
+        then
+            break
+        fi
+        cd ..
     done
     g=`pwd`
     g=${g##*$prefix}
@@ -112,13 +110,13 @@ echo Build each grammar changed in PR.
 for ((i=0; i<${#prs[@]}; i++))
 do
     rm -rf "$cwd/${prs[$i]}"
-	mkdir "$cwd/${prs[$i]}"
-	git checkout ${com[$i]}
+    mkdir "$cwd/${prs[$i]}"
+    git checkout ${com[$i]}
     for g in ${tests[@]}
     do
         echo Grammar $g
         pushd $g
-	    gg=`echo $g | tr '/' '-'`
+        gg=`echo $g | tr '/' '-'`
         if [ "$local" == "" ]
         then
             trgen -t CSharp
@@ -130,9 +128,8 @@ do
         cd $where
         make
         popd
-		cp -r $g "$cwd/${prs[$i]}/$gg"
+    cp -r $g "$cwd/${prs[$i]}/$gg"
     done
-    popd
 done
 
 #===========================
@@ -142,54 +139,58 @@ do
     echo Grammar $g
     rm -f "$cwd"/p[0-1]*
     gg=`echo $g | tr '/' '-'`
+    what=()
     for ((i=0; i<${#prs[@]}; i++))
     do
         pushd "$cwd/${prs[$i]}/$gg"
         where=`echo Generated-CSharp* | tr ' ' '\n' | head -1`
         echo $where
         cd $where
-        if [ "$local" == "" ]
+        if [ "${#what[@]}" -eq 0 ]
         then
-            what=`trxml2 desc.xml | grep inputs | head -1 | sed 's%^[^=]*=%%'`
-        else
-            what=`dotnet trxml2 -- desc.xml | grep inputs | head -1 | sed 's%^[^=]*=%%'`
-        fi
-        if [ "$what" == "" ]
-        then
-            dir=`pwd`
-            p=`realpath -s --relative-to=$dir "$cwd/${prs[1]}/$g/examples"`
             if [ "$local" == "" ]
             then
-                what=( `trglob $p | grep -v '.errors$' | grep -v '.tree$'` )
+                what=`trxml2 desc.xml | grep inputs | head -1 | sed 's%^[^=]*=%%'`
             else
-                what=( `dotnet trglob -- $p | grep -v '.errors$' | grep -v '.tree$'` )
+                what=`dotnet trxml2 -- desc.xml | grep inputs | head -1 | sed 's%^[^=]*=%%'`
             fi
-        else
-            dir=`pwd`
-            p=`realpath -s --relative-to=$dir "$cwd/${prs[1]}/$g/$what"`
-            if [ "$local" == "" ]
+            if [ "$what" == "" ]
             then
-                what=( `trglob $p | grep -v '.errors$' | grep -v '.tree$'` )
+                dir=`pwd`
+                p=`realpath -s --relative-to=$dir "$cwd/${prs[1]}/$gg/examples"`
+                if [ "$local" == "" ]
+                then
+                    what=( `trglob $p | grep -v '.errors$' | grep -v '.tree$'` )
+                else
+                    what=( `dotnet trglob -- $p | grep -v '.errors$' | grep -v '.tree$'` )
+                fi
             else
-                what=( `dotnet trglob -- $p | grep -v '.errors$' | grep -v '.tree$'` )
+                dir=`pwd`
+                p=`realpath -s --relative-to=$dir "$cwd/${prs[1]}/$gg/$what"`
+                if [ "$local" == "" ]
+                then
+                    what=( `trglob $p | grep -v '.errors$' | grep -v '.tree$'` )
+                else
+                    what=( `dotnet trglob -- $p | grep -v '.errors$' | grep -v '.tree$'` )
+                fi
             fi
+            echo what = $what
+            newwhat=()
+            for f in ${what[@]}
+            do
+                if [ -d $f ]; then continue; fi
+                newwhat=( ${newwhat[@]} $f )
+            done
+            what=( ${newwhat[@]} )
+            if [ ${#what[@]} -eq 0 ]; then popd; continue; fi
         fi
-        echo what = $what
-        newwhat=()
-        for f in ${what[@]}
-        do
-            if [ -d $f ]; then continue; fi
-            newwhat=( ${newwhat[@]} $f )
-        done
-        what=( ${newwhat[@]} )
-        if [ ${#what[@]} -eq 0 ]; then popd; continue; fi
         for j in {1..40}
         do
             bash run.sh ${what[@]} 2>&1 | grep "Total Time" | awk '{print $3}' >> "$cwd/p$i-$gg.txt"
         done
         popd
     done
-	
+    
     echo Graphing out.
     cd $cwd
     rm -f xx.m
@@ -238,29 +239,29 @@ do
     done
     echo " ];" >> xx.m
     cat >> xx.m <<EOF
-		bar(x,data);
-		set(gca, 'XTickLabel', str, 'XTick', 1:numel(x));
-		hold on
-		er = errorbar(x,data,errlow,errhigh);
-		hold off
-		set(er, "color", [0 0 0])
-		set(er, "linewidth", 3);
-		set(er, "linestyle", "none");
-		set(gca, "fontsize", 6)
-		xlabel("Target");
-		ylabel("Runtime (s)");
-		title("Comparison of Runtimes")
-		print("./times-$gg.svg", "-dsvg")
-		[pval, t, df] = welch_test(p0, p1)
-		if (abs(pval) < 0.03 && mp0/mp1 > 1.05)
-		  printf("The PR statistically and practically decreased performance for $gg.\n");
-		else
-		  printf("The PR did not signficantly negatively alter performance for $gg.\n");
-		endif
+        bar(x,data);
+        set(gca, 'XTickLabel', str, 'XTick', 1:numel(x));
+        hold on
+        er = errorbar(x,data,errlow,errhigh);
+        hold off
+        set(er, "color", [0 0 0])
+        set(er, "linewidth", 3);
+        set(er, "linestyle", "none");
+        set(gca, "fontsize", 6)
+        xlabel("Target");
+        ylabel("Runtime (s)");
+        title("Comparison of Runtimes")
+        print("./times-$gg.svg", "-dsvg")
+        [pval, t, df] = welch_test(p0, p1)
+        if (abs(pval) < 0.03 && mp0/mp1 > 1.05)
+          printf("The PR statistically and practically decreased performance for $gg.\n");
+        else
+          printf("The PR did not signficantly negatively alter performance for $gg.\n");
+        endif
 EOF
-	echo ========
-	cat xx.m
-	echo ========
+    echo ========
+    cat xx.m
+    echo ========
     cat xx.m | octave --no-gui
 
 done
