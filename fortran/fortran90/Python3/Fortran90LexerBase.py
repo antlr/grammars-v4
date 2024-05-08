f7f1c1da522540bb0a89bffadb05a3a72da275a5
Ken Domino <kenneth.domino@domemtech.com>
2024-05-08 08:26:08 -0400

    Update Fortran90LexerBase.py

--- fortran/fortran90/Python3/Fortran90LexerBase.py
+++ fortran/fortran90/Python3/Fortran90LexerBase.py
@@ -5,19 +5,19 @@ class Fortran90LexerBase(Lexer):
         return self.column == 0
 
     def VerifyNotOperator():
-        var c1 = this.InputStream.LA(1);
+        c1 = this.InputStream.LA(1);
         if (c1 == 'a'):
-            var c2 = this.InputStream.LA(2);
+            c2 = this.InputStream.LA(2);
             if (c2 == 'n'):
-                var c3 = this.InputStream.LA(3);
+                c3 = this.InputStream.LA(3);
                 if (c3 == 'd'):
-                    var c4 = this.InputStream.LA(4);
+                    c4 = this.InputStream.LA(4);
                     if (c4 == '.'):
                         return false;
-        else if (c1 == 'o'):
-            var c2 = this.InputStream.LA(2);
+        elif (c1 == 'o'):
+            c2 = this.InputStream.LA(2);
             if (c2 == 'r'):
-                var c3 = this.InputStream.LA(3);
+                c3 = this.InputStream.LA(3);
                 if (c3 == '.'):
                     return false;
         return true;
