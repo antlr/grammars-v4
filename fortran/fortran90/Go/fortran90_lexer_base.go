4e6f3e216384d86af50c91991a646228d251e0a6
Ken Domino <kenneth.domino@domemtech.com>
2024-05-08 09:44:39 -0400

    Update Go port, remove PHP port.

--- fortran/fortran90/Go/fortran90_lexer_base.go
+++ fortran/fortran90/Go/fortran90_lexer_base.go
@@ -14,27 +14,26 @@ func (l *Fortran90LexerBase) IsColumnZero() bool {
 }
 
 func (l *Fortran90LexerBase) VerifyNotOperator() bool {
-    var c1 = this.InputStream.LA(1);
+    c1 := l.GetInputStream().(antlr.CharStream).LA(1);
     if (c1 == 'a') {
-        var c2 = this.InputStream.LA(2);
+        c2 := l.GetInputStream().(antlr.CharStream).LA(2);
         if (c2 == 'n') {
-            var c3 = this.InputStream.LA(3);
+            c3 := l.GetInputStream().(antlr.CharStream).LA(3);
             if (c3 == 'd') {
-                var c4 = this.InputStream.LA(4);
+                c4 := l.GetInputStream().(antlr.CharStream).LA(4);
                 if (c4 == '.') {
                     return false;
                 }
             }
         }
-    }
-    else if (c1 == 'o') {
-        var c2 = this.InputStream.LA(2);
+    } else if (c1 == 'o') {
+        c2 := l.GetInputStream().(antlr.CharStream).LA(2);
         if (c2 == 'r') {
-            var c3 = this.InputStream.LA(3);
+            c3 := l.GetInputStream().(antlr.CharStream).LA(3);
             if (c3 == '.') {
                 return false;
             }
         }
-        return true;
     }
+    return true;
 }
