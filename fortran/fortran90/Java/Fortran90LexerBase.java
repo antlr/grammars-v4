4b42f4a435285ad2c3a2a1b3f072d40c8236dd5c
Ken Domino <kenneth.domino@domemtech.com>
2024-05-08 07:44:58 -0400

    Update Fortran90LexerBase.java

--- fortran/fortran90/Java/Fortran90LexerBase.java
+++ fortran/fortran90/Java/Fortran90LexerBase.java
@@ -12,18 +12,18 @@ public abstract class Fortran90LexerBase extends Lexer
     return this.getCharPositionInLine() == 0;
     }
 
-    public bool VerifyNotOperator()
+    public boolean VerifyNotOperator()
     {
-        var c1 = this.InputStream.LA(1);
+        var c1 = this.getInputStream().LA(1);
         if (c1 == 'a')
         {
-            var c2 = this.InputStream.LA(2);
+            var c2 = this.getInputStream().LA(2);
             if (c2 == 'n')
             {
-                var c3 = this.InputStream.LA(3);
+                var c3 = this.getInputStream().LA(3);
                 if (c3 == 'd')
                 {
-                    var c4 = this.InputStream.LA(4);
+                    var c4 = this.getInputStream().LA(4);
                     if (c4 == '.')
                     {
                         return false;
@@ -33,10 +33,10 @@ public abstract class Fortran90LexerBase extends Lexer
         }
         else if (c1 == 'o')
         {
-            var c2 = this.InputStream.LA(2);
+            var c2 = this.getInputStream().LA(2);
             if (c2 == 'r')
             {
-                var c3 = this.InputStream.LA(3);
+                var c3 = this.getInputStream().LA(3);
                 if (c3 == '.')
                 {
                     return false;
