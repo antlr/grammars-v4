1ce834a357029843461ef447a20c61518637d61a
Mike Lischke <mike.lischke@oracle.com>
2024-05-19 15:16:55 +0200

    Simplified copyrights

--- sql/mysql/Oracle/TypeScript/demo.ts
+++ sql/mysql/Oracle/TypeScript/demo.ts
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) Mike Lischke. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
+ * Copyright © 2024, Oracle and/or its affiliates
  */
 
 /* cspell: ignore antlr, armscii, keybcs, ujis, macce, macroman, geostd, eucjpms, sakila */
