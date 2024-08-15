7138246bc0a6eeb7b5b25bfc12882fbff7d76b65
Webstones MacBook <23607165@sun.ac.za>
2024-08-15 08:03:28 +0200

    Changes applied as requested

--- rust/Python3/RustLexerBase.py
+++ rust/Python3/RustLexerBase.py
@@ -3,9 +3,41 @@ from antlr4 import *
 from antlr4.InputStream import InputStream
 
 class RustLexerBase(Lexer):
+    flt_mp = set()
+    RustLexer = None
     def __init__(self, input: InputStream, output: TextIO = ...) -> None:
         super().__init__(input, output)
         self.token_lookbehind: tuple[Optional[Token], Optional[Token]] = (None, None)
+        try:
+            from .RustLexer import RustLexer
+        except ImportError:
+            from RustLexer import RustLexer
+        RustLexerBase.RustLexer = RustLexer
+        RustLexerBase.flt_mp = {
+                RustLexer.STRING_LITERAL,
+                RustLexer.RAW_STRING_LITERAL,
+                RustLexer.BYTE_LITERAL,
+                RustLexer.BYTE_STRING_LITERAL,
+                RustLexer.RAW_BYTE_STRING_LITERAL,
+                RustLexer.INTEGER_LITERAL,
+                RustLexer.DEC_LITERAL,
+                RustLexer.HEX_LITERAL,
+                RustLexer.OCT_LITERAL,
+                RustLexer.BIN_LITERAL,
+                RustLexer.KW_SUPER,
+                RustLexer.KW_SELFVALUE,
+                RustLexer.KW_SELFTYPE,
+                RustLexer.KW_CRATE,
+                RustLexer.KW_DOLLARCRATE,
+                RustLexer.RCURLYBRACE,
+                RustLexer.RSQUAREBRACKET,
+                RustLexer.RPAREN,
+                RustLexer.KW_AWAIT,
+                RustLexer.NON_KEYWORD_IDENTIFIER,
+                RustLexer.RAW_IDENTIFIER,
+                RustLexer.KW_MACRORULES,   
+                RustLexer.GT 
+            }
 
         """LOOK BEHIND TOKENS"""
     def nextToken(self):
@@ -48,39 +80,11 @@ class RustLexerBase(Lexer):
         return True
 
     def floatLiteralPossible(self):
-        try:
-            from .RustLexer import RustLexer
-        except ImportError:
-            from RustLexer import RustLexer
         prev, current = self.token_lookbehind
         
         if prev == None or current == None:
             return True
-        elif current.type != RustLexer.DOT:
+        elif current.type != RustLexerBase.RustLexer.DOT:
             return True
         else:
-            return prev.type not in {
-            RustLexer.STRING_LITERAL,
-            RustLexer.RAW_STRING_LITERAL,
-            RustLexer.BYTE_LITERAL,
-            RustLexer.BYTE_STRING_LITERAL,
-            RustLexer.RAW_BYTE_STRING_LITERAL,
-            RustLexer.INTEGER_LITERAL,
-            RustLexer.DEC_LITERAL,
-            RustLexer.HEX_LITERAL,
-            RustLexer.OCT_LITERAL,
-            RustLexer.BIN_LITERAL,
-            RustLexer.KW_SUPER,
-            RustLexer.KW_SELFVALUE,
-            RustLexer.KW_SELFTYPE,
-            RustLexer.KW_CRATE,
-            RustLexer.KW_DOLLARCRATE,
-            RustLexer.RCURLYBRACE,
-            RustLexer.RSQUAREBRACKET,
-            RustLexer.RPAREN,
-            RustLexer.KW_AWAIT,
-            RustLexer.NON_KEYWORD_IDENTIFIER,
-            RustLexer.RAW_IDENTIFIER,
-            RustLexer.KW_MACRORULES,   
-            RustLexer.GT 
-        }
+            return prev.type not in RustLexerBase.flt_mp 
