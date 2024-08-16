6c4f82763f027b947472d6cd909091298ce2a691
Ken Domino <kenneth.domino@domemtech.com>
2024-08-15 22:58:46 -0400

    Fix #4205 (#4206)
    Moved require/include alts below arithmetic alts, below spaceship alt. Changed name of alt label to conform to spec https://github.com/php/php-langspec/blob/master/spec/19-grammar.md#grammar-include-expression

---
+++ php/examples/require-include.php
@@ -0,0 +1,7 @@
+<?php
+   require './config.php';
+   require "./config.php";
+   require "." . "/config.php";
+   require_once "./config.php";
+   include "./config.php";
+   include_once "." . "/config.php";
