40d05e2cc2513d9f71a2d63229734c3dfff8b02e
Ken Domino <kenneth.domino@domemtech.com>
2024-09-27 06:49:53 -0400

    Fix #3314

--- javascript/typescript/TypeScript/TypeScriptLexerBase.ts
+++ javascript/typescript/TypeScript/TypeScriptLexerBase.ts
@@ -1,11 +1,11 @@
-import {Lexer, Token, CharStream} from "antlr4ts";
-import {TypeScriptLexer} from './TypeScriptLexer';
+import {Lexer, Token, CharStream} from "antlr4";
+import TypeScriptLexer from './TypeScriptLexer';
 
 /**
  * All lexer methods that used in grammar (IsStrictMode)
  * should start with Upper Case Char similar to Lexer rules.
  */
-export abstract class TypeScriptLexerBase extends Lexer {
+export default abstract class TypeScriptLexerBase extends Lexer {
     /**
      * Stores values of nested modes. By default mode is strict or
      * defined externally (useStrictDefault)
