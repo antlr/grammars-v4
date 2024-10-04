1fd4503a67447a9a54b925991bb0625a14702492
Rafed Muhammad Yasir <rafed123@gmail.com>
2024-10-04 14:59:43 -0700

    Add TypeScript target for Golang (#4269)
    * Add TypeScript target for Golang
    
    * Remove console.log

---
+++ golang/TypeScript/GoParserBase.ts
@@ -0,0 +1,15 @@
+import { Parser, TokenStream, BufferedTokenStream } from 'antlr4';
+import GoLexer from './GoLexer';
+
+export default abstract class GoParserBase extends Parser {
+  constructor(input: TokenStream) {
+    super(input);
+  }
+
+  protected closingBracket(): boolean {
+    const stream = this._input as BufferedTokenStream;
+    const prevTokenType = stream.LA(1);
+
+    return prevTokenType === GoLexer.R_CURLY || prevTokenType === GoLexer.R_PAREN;
+  }
+}
