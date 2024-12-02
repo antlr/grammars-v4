e9d22d205fab75d045aa727222fe19c901cf1ad9
Ken Domino <kenneth.domino@domemtech.com>
2024-12-02 07:50:56 -0500

    Split actions to different lines so transformGrammar.py doesn't need rewrite. Fix Go target (not working yet).

--- sql/mysql/Oracle/Go/MySQLParserBase.go
+++ sql/mysql/Oracle/Go/MySQLParserBase.go
@@ -5,5 +5,43 @@ import (
 )
 
 type MySQLParserBase struct {
-	*antlr.BaseParser
+    *antlr.BaseParser
+    serverVersion int
+    sqlModes           map[SqlMode]bool
+    supportMle bool
 }
+
+func NewMySQLParserBase(input antlr.InputStream) *MySQLParserBase {
+    r := &MySQLParserBase{
+        supportMle:     true,
+        serverVersion: 80200,
+    }
+    r.sqlModes = make(map[SqlMode]bool)
+    return r
+}
+
+// isSqlModeActive determines if the given SQL mode is currently active in the lexer.
+func (m *MySQLParserBase) isSqlModeActive(mode SqlMode) bool {
+    return m.sqlModes[mode]
+}
+
+// isPureIdentifier checks if the lexer is in ANSI_QUOTES mode.
+func (m *MySQLParserBase) isPureIdentifier() bool {
+    return m.isSqlModeActive(AnsiQuotes)
+}
+
+// isTextStringLiteral checks if the lexer is not in ANSI_QUOTES mode.
+func (m *MySQLParserBase) isTextStringLiteral() bool {
+    return !m.isSqlModeActive(AnsiQuotes)
+}
+
+// isStoredRoutineBody checks if the server version supports stored routine body.
+func (m *MySQLParserBase) isStoredRoutineBody() bool {
+    return m.serverVersion >= 80032 && m.supportMle
+}
+
+// isSelectStatementWithInto checks if the server version supports SELECT INTO syntax.
+func (m *MySQLParserBase) isSelectStatementWithInto() bool {
+    return m.serverVersion >= 80024 && m.serverVersion < 80031
+}
+
