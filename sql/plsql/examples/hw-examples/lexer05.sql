2676c09bab0331b7c5f7405574b9d58c5263cbf9
Ken Domino <kenneth.domino@domemtech.com>
2024-12-20 06:07:10 -0500

    Reorganize examples.

--- sql/plsql/examples/hw-examples/lexer05.sql
+++
@@ -1,1 +0,0 @@
-select tbl$or$idx$part$num("sys"."wrh:_seg_stat",0,4,0,"rowid") as c1 from t1
--- sql/plsql/examples/lexer02.sql
+++ sql/plsql/examples/lexer02.sql
@@ -1,7 +1,2 @@
-select 'A' | | 'B'  from dual;
+select 'A' | | 'B'  from dual
 
-select a ä, b Ḅ from dual;
-
-CREATE OR REPLACE PACKAGE ACCESS_SECURITY AS
--- omitted
-END;
