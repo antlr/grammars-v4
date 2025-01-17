724a7a68ca64cbe9cd21c6a45671c5213bcd3e19
Ken Domino <kenneth.domino@domemtech.com>
2025-01-17 09:14:59 -0500

    Updates to fix Cpp build.
    * Move code to init npm environment after cd'ing to build.
    * Remove code to get version. It doesn't work because Antlr-ng and Antlr4 tools are very different in version strings.
    * Remove "-no-listener" because it's useless, but also crashes Antlr-ng.

--- _scripts/templates/Cpp/cmake/st.FindANTLR.cmake
+++ _scripts/templates/Cpp/cmake/st.FindANTLR.cmake
@@ -1,26 +1,5 @@
 find_package(Java QUIET COMPONENTS Runtime)
 
-  execute_process(
-<if(antlrng_tool)>
-      COMMAND node node_modules/antlr-ng/dist/cli/runner.js --version
-<else>
-      COMMAND antlr4<if(os_win)>.exe<else><endif> -v ${ANTLR4_TAG}
-<endif>
-      OUTPUT_VARIABLE ANTLR_COMMAND_OUTPUT
-      ERROR_VARIABLE ANTLR_COMMAND_ERROR
-      RESULT_VARIABLE ANTLR_COMMAND_RESULT
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-  if(ANTLR_COMMAND_RESULT EQUAL 0)
-    string(REGEX MATCH "Version [0-9]+(\\\\.[0-9]+)*" ANTLR_VERSION ${ANTLR_COMMAND_OUTPUT})
-    string(REPLACE "Version " "" ANTLR_VERSION ${ANTLR_VERSION})
-  else()
-    message(
-        SEND_ERROR
-        "Command antlr4<if(os_win)>.exe<else><endif> "
-        "failed with the output '${ANTLR_COMMAND_ERROR}'")
-  endif()
-
   macro(ANTLR_TARGET Name InputFile)
     set(ANTLR_OPTIONS LEXER PARSER LISTENER VISITOR)
     set(ANTLR_ONE_VALUE_ARGS PACKAGE OUTPUT_DIRECTORY DEPENDS_ANTLR)
@@ -108,12 +87,11 @@ find_package(Java QUIET COMPONENTS Runtime)
 <endif>
                 ${InputFile}
                 -o ${ANTLR_${Name}_OUTPUT_DIR}
-                -no-listener
                 -Dlanguage=Cpp
                 ${ANTLR_TARGET_COMPILE_FLAGS}
         DEPENDS ${InputFile}
                 ${ANTLR_TARGET_DEPENDS}
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        COMMENT "Building ${Name} with ANTLR ${ANTLR_VERSION}")
+        COMMENT "Building ${Name} with ANTLR")
   endmacro(ANTLR_TARGET)
 
--- _scripts/templates/Cpp/st.build.sh
+++ _scripts/templates/Cpp/st.build.sh
@@ -2,13 +2,13 @@
 set -e
 if [ -f transformGrammar.py ]; then python3 transformGrammar.py ; fi
 rm -rf node_modules
+rm -rf build
+mkdir build
+cd build
 <if(antlrng_tool)>
 npm init -y
 npm i antlr-ng
 <endif>
-rm -rf build
-mkdir build
-cd build
 <if(test.IsWindows)>cmake .. -G "Visual Studio 17 2022" -A x64
 <else>cmake ..
 <endif>
