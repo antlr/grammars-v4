10470665022adbf08ba32366fbc5329c186b6a34
Ken Domino <kenneth.domino@domemtech.com>
2025-04-05 17:28:18 -0400

    Add Python3 port.

--- golang/Python3/GoParserBase.py
+++ golang/Python3/GoParserBase.py
@@ -1,15 +1,96 @@
+import sys
 from antlr4 import *
+if sys.version_info[1] > 5:
+    from typing import TextIO
+else:
+    from typing.io import TextIO
 
 class GoParserBase(Parser):
 
-    def closingBracket(self) -> bool:
-        la = self._input.LA(1)
-        return la == self.R_PAREN or la == self.R_CURLY or la == Token.EOF
+    debug = False
 
-    def isType(self) -> bool:
-        la = self._input.LA(1)
-        return la != self.IDENTIFIER
+    def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.table = set()
+
+    def myreset(self):
+        la = 1
+    
+    def closingBracket(self) -> bool:
+        la = self._input.LT(1)
+        return la.type == self.R_PAREN or la.type == self.R_CURLY or la.type == Token.EOF
 
     def isNotReceive(self) -> bool:
-        la = self._input.LA(2)
-        return la != self.RECEIVE
+        la = self._input.LT(2)
+        return la.type != self.RECEIVE
+
+    def addImportSpec(self):
+        ctx = self._ctx
+        if not isinstance(ctx, self.ImportSpecContext):
+            return
+        importSpec = ctx
+        if importSpec.packageName() is not None:
+            name = importSpec.packageName().getText()
+            if self.debug:
+                print("Entering", name)
+            self.table.add(name)
+        else:
+            name = importSpec.importPath().getText()
+            name = name.replace("\"", "")
+            name = name.replace("\\", "/")
+            path_arr = name.split('/')
+            file_arr = path_arr[-1].split('.')
+            file_name = file_arr[-1]
+            if self.debug:
+                print("Entering", file_name)
+            self.table.add(file_name)
+
+    def isOperand(self) -> bool:
+        la = self._input.LT(1)
+        if la.text == "err":
+            return True
+        result = True
+        if la.type != self.IDENTIFIER:
+            if self.debug:
+                print(f"isOperand Returning {result} for {la.text}")
+            return result
+        result = (la.text in self.table)
+        la2 = self._input.LT(2)
+        if la2.type != self.DOT:
+            result = True
+            if self.debug:
+                print(f"isOperand Returning {result} for {la.text}")
+            return result
+        la3 = self._input.LT(3)
+        if la3.type == self.L_PAREN:
+            result = True
+            if self.debug:
+                print(f"isOperand Returning {result} for {la.text}")
+            return result
+        if self.debug:
+            print(f"isOperand Returning {result} for {la.text}")
+        return result
+
+    def isConversion(self) -> bool:
+        la = self._input.LT(1)
+        result = (la.type != self.IDENTIFIER)
+        if self.debug:
+            print(f"isConversion Returning {result} for {la.text}")
+        return result
+
+    def isMethodExpr(self) -> bool:
+        la = self._input.LT(1)
+        result = True
+        if la.type == self.STAR:
+            if self.debug:
+                print(f"isMethodExpr Returning {result} for {la.text}")
+            return result
+        if la.type != self.IDENTIFIER:
+            result = False
+            if self.debug:
+                print(f"isMethodExpr Returning {result} for {la.text}")
+            return result
+        result = (la.text not in self.table)
+        if self.debug:
+            print(f"isMethodExpr Returning {result} for {la.text}")
+        return result
