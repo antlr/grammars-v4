3033439b3561d2ba9ce73bcb8488567725be4926
Ken Domino <kenneth.domino@domemtech.com>
2025-07-10 20:36:47 -0400

    Update .errors files.
    These files are produced because if a field type is not declared, it cannot be either an enum or message type. Probably to restrictive.

---
+++ protobuf/protobuf3/CSharp/Protobuf3ParserBase.cs
@@ -0,0 +1,116 @@
+using Antlr4.Runtime;
+using System.Diagnostics;
+using System.IO;
+
+public abstract class Protobuf3ParserBase : Parser
+{
+    private readonly ITokenStream _input;
+    private bool debug = false;
+    private const string prefix = "   ";
+    SymbolTable symbolTable = new SymbolTable();
+
+    public Protobuf3ParserBase(ITokenStream input, TextWriter output, TextWriter errorOutput)
+        : base(input, output, errorOutput)
+    {
+        _input = input;
+    }
+
+    public void DoMessageNameDef_()
+    {
+        var ctx = this.Context;
+        var tctx = (Protobuf3Parser.DoMessageNameDefContext)ctx;
+        var identifier = ((Protobuf3Parser.MessageDefContext)(tctx.Parent)).messageName().ident();
+        var name = identifier.GetText();
+        var current = symbolTable.CurrentScope();
+        var sym = new Symbol() { Name = name, Type = current, Classification = TypeClassification.Message_ };
+        symbolTable.Define(sym);
+        if (debug) System.Console.WriteLine(prefix + "defined Message " + sym);
+    }
+
+    public void DoEnumNameDef_()
+    {
+        var ctx = this.Context;
+        var tctx = (Protobuf3Parser.DoEnumNameDefContext)ctx;
+        var identifier = ((Protobuf3Parser.EnumDefContext)(tctx.Parent)).enumName().ident();
+        var name = identifier.GetText();
+        var current = symbolTable.CurrentScope();
+        var sym = new Symbol() { Name = name, Type = current, Classification = TypeClassification.Enum_ };
+        symbolTable.Define(sym);
+        if (debug) System.Console.WriteLine(prefix + "defined Enum " + sym);
+    }
+
+    public void DoServiceNameDef_()
+    {
+        var ctx = this.Context;
+        var tctx = (Protobuf3Parser.DoServiceNameDefContext)ctx;
+        var identifier = ((Protobuf3Parser.ServiceDefContext)(tctx.Parent)).serviceName().ident();
+        var name = identifier.GetText();
+        var current = symbolTable.CurrentScope();
+        var sym = new Symbol() { Name = name, Type = current, Classification = TypeClassification.Service_ };
+        symbolTable.Define(sym);
+        if (debug) System.Console.WriteLine(prefix + "defined Service " + sym);
+    }
+
+    public void DoEnterBlock_()
+    {
+        var newScope = new Symbol() { Name = "<block>", Classification = TypeClassification.Block_ };
+        if (debug) System.Console.WriteLine(prefix + "EnterBlock " + newScope);
+        symbolTable.EnterScope(newScope);
+    }
+
+    public void DoExitBlock_()
+    {
+        if (debug) System.Console.WriteLine(prefix + "ExitBlock " + symbolTable.CurrentScope());
+        symbolTable.ExitScope();
+    }
+
+    public bool IsMessageType_()
+    {
+//        var ctx = this.Context;
+//        System.Diagnostics.Debug.Assert(ctx is Protobuf3Parser.MessageTypeContext);
+//        var tctx = ctx as Protobuf3Parser.MessageTypeContext;
+
+	var la = tokenStream.LT(1);
+	var id = la.Text;
+
+//        var mn = tctx.messageName();
+//        var id = mn.GetText();
+//	System.Console.WriteLine("IsMessageType " + id);
+        var symbol = symbolTable.Resolve(id);
+        if (symbol != null)
+        {
+            if (symbol.Classification == TypeClassification.Message_)
+                return true;
+        }
+        return false;
+    }
+
+    public bool IsEnumType_()
+    {
+//        var ctx = this.Context;
+//        System.Diagnostics.Debug.Assert(ctx is Protobuf3Parser.EnumTypeContext);
+//        var tctx = ctx as Protobuf3Parser.EnumTypeContext;
+
+	var la = tokenStream.LT(1);
+	var id = la.Text;
+
+//        var mn = tctx.enumName();
+//        var id = mn.GetText();
+//	System.Console.WriteLine("IsEnumType " + id);
+        var symbol = symbolTable.Resolve(id);
+        if (symbol != null)
+        {
+            if (symbol.Classification == TypeClassification.Enum_)
+                return true;
+        }
+        return false;
+    }
+
+    private ITokenStream tokenStream
+    {
+	    get
+	    {
+		    return TokenStream;
+	    }
+    }
+}
