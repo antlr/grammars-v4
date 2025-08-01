4122cfbb84f6639411cea10ba4f944aa80e16648
Ken Domino <kenneth.domino@domemtech.com>
2025-08-01 06:03:21 -0400

    Clean up.

--- _scripts/maven.sh
+++ _scripts/maven.sh
@@ -1,28 +1,13 @@
 #!/bin/sh
 #set -x
 #set -e
-#echo "YO. I AM HERE."
 
 grammars=()
-
-# Test grammars for the enclosing directories.
-#echo "Files."
-#git diff --name-only $1 $2 -- .
-#echo "f1."
-#git diff --name-only $1 $2 -- . 2> /dev/null | sed 's#\(.*\)[/][^/]*$#\1#'
-#echo "f2."
-#git diff --name-only $1 $2 -- . 2> /dev/null | sed 's#\(.*\)[/][^/]*$#\1#' | sort -u
-#echo "f3."
-#git diff --name-only $1 $2 -- . 2> /dev/null | sed 's#\(.*\)[/][^/]*$#\1#' | sort -u | grep -v _scripts
-#echo "ENd Files."
-
 directories=`git diff --name-only $1 $2 -- . 2> /dev/null | sed 's#\(.*\)[/][^/]*$#\1#' | sort -u | grep -v _scripts | grep -v .github`
-echo "Yo. Directories is $directories"
 prefix=`pwd`
 for g in $directories
 do
     pushd $g
-    pwd
     while true
     do
 	if [ `pwd` == '/' ]
