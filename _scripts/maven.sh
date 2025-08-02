be4e063bb1d49e0abd60f72e689e5b33e575336b
Ken Domino <kenneth.domino@domemtech.com>
2025-08-02 05:07:19 -0400

    Fix various malformed grammar tests, or disable.

--- _scripts/maven.sh
+++ _scripts/maven.sh
@@ -3,31 +3,42 @@
 #set -e
 
 grammars=()
-directories=`git diff --name-only $1 $2 -- . 2> /dev/null | sed 's#\(.*\)[/][^/]*$#\1#' | sort -u | grep -v _scripts | grep -v .github`
+directories=`git diff --name-only $1 $2 -- . 2> /dev/null | sed 's#\(.*\)[/][^/]*$#\1#' | sort -u | grep -v _scripts | grep -v .github | grep -v '^[.]$' | grep -v target | grep -v Generated`
 prefix=`pwd`
 for g in $directories
 do
     pushd $g
     while true
     do
-	if [ `pwd` == '/' ]
-	then
-	    break
-	elif [ -f pom.xml ]
-	then
-	    break
-	elif [ `pwd` == "$prefix" ]
-	then
-	    break
-	fi
-	cd ..
+        if [ `pwd` == '/' ]
+        then
+            break
+        elif [ -f pom.xml ]
+        then
+            break
+        elif [ `pwd` == "$prefix" ]
+        then
+            break
+        fi
+        cd ..
     done
     g=`pwd`
     g=${g##*$prefix}
     g=${g##/}
     if [ "$g" == "" ]
     then
-	g="."
+        g="."
+    fi
+    if [ ! -f pom.xml ]
+    then
+        popd > /dev/null
+        continue
+    fi
+    grep -q -e '<modules>' pom.xml
+    if [ $? -eq 0 ]
+    then
+        popd > /dev/null
+        continue
     fi
     popd > /dev/null
     grammars+=( $g )
