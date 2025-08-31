37146747969be81255787b80d476873ec24d2626
Ken Domino <kenneth.domino@domemtech.com>
2025-08-31 14:56:52 -0400

    [java] Fix for #4601: large max-k for expressions in annotation values. (#4608)
    * Correct formalParameters rule.
    
    * Fix grammar formalParameter.
    
    Ellipsis as last parameter should not be enforced by grammar. It causes max-k explosion. For now, do what the javac compiler does.
    
    * Correct several parser rules.
    
    Fix classOrInterfaceType, classType, typeIdentifier.
    
    * Remaster tests.
    
    * Remaster.
    
    * Corrected refactoring.
    
    From spec:
    
    classOrInterfaceType:
        classType
        | interfaceType
        ;
    
    interfaceType:
        classType
        ;
    
    classType:
        annotation* typeIdentifier typeArguments?
        | packageName '.' annotation* typeIdentifier typeArguments?
        | classOrInterfaceType '.' annotation* typeIdentifier typeArguments?
        ;
    
    ====
    unfold interfaceType, delete interfaceType (no type checker)
    
    classOrInterfaceType:
        classType
        | classType
        ;
    
    interfaceType:
        classType
        ;
    
    classType:
        annotation* typeIdentifier typeArguments?
        | packageName '.' annotation* typeIdentifier typeArguments?
        | classOrInterfaceType '.' annotation* typeIdentifier typeArguments?
        ;
    
    =====
    group classType (again, no type checker)
    
    classOrInterfaceType:
        classType
        ;
    
    classType:
        annotation* typeIdentifier typeArguments?
        | packageName '.' annotation* typeIdentifier typeArguments?
        | classOrInterfaceType '.' annotation* typeIdentifier typeArguments?
        ;
    
    ====
    We are required to hoist out annotation*. It's not correct, but will be ok.
    
    classType:
        typeIdentifier typeArguments?
        | packageName '.' annotation* typeIdentifier typeArguments?
        | classOrInterfaceType '.' annotation* typeIdentifier typeArguments?
        ;
    
    ====
    unfold classOrInterfaceType
    
    classType:
        typeIdentifier typeArguments?
        | packageName '.' annotation* typeIdentifier typeArguments?
        | classType '.' annotation* typeIdentifier typeArguments?
        ;
    
    =====
    Replace left recursion in classOrInterfaceType with Kleene-closure.
    
    z -> a | b | z c
    =>
    z -> (a | b)+ c*
    
    classType:
        (
          typeIdentifier typeArguments?
          | packageName '.' annotation* typeIdentifier typeArguments?
        )+ ( '.' annotation* typeIdentifier typeArguments? )*
        ;
    
    =====
    group
    
    classType:
        (
          ( | packageName '.' annotation* ) typeIdentifier typeArguments?
        )+ ( '.' annotation* typeIdentifier typeArguments? )*
        ;
    
    ====
    refactor as ?-operator
    
    classType:
        (
          ( packageName '.' annotation* )? typeIdentifier typeArguments?
        )+ ( '.' annotation* typeIdentifier typeArguments? )*
        ;
    
    * Remaster.
    
    * Fix for #4601

--- java/java/CSharp/JavaParserBase.cs
+++ java/java/CSharp/JavaParserBase.cs
@@ -25,4 +25,37 @@ public abstract class JavaParserBase : Parser {
         }
         return true;
     }
+
+    public bool IsNotIdentifierAssign()
+    {
+	    var la = this.TokenStream.LA(1);
+	    // If not identifier, return true because it can't be
+	    // "identifier = ..."
+	    switch (la) {
+		    case JavaParser.IDENTIFIER:
+		    case JavaParser.MODULE:
+		    case JavaParser.OPEN:
+		    case JavaParser.REQUIRES:
+		    case JavaParser.EXPORTS:
+		    case JavaParser.OPENS:
+		    case JavaParser.TO:
+		    case JavaParser.USES:
+		    case JavaParser.PROVIDES:
+		    case JavaParser.WHEN:
+		    case JavaParser.WITH:
+		    case JavaParser.TRANSITIVE:
+		    case JavaParser.YIELD:
+		    case JavaParser.SEALED:
+		    case JavaParser.PERMITS:
+		    case JavaParser.RECORD:
+		    case JavaParser.VAR:
+			    break;
+		    default:
+			    return true;
+	    }
+	    var la2 = this.TokenStream.LA(2);
+	    if (la2 != JavaParser.ASSIGN) return true;
+	    return false;
+	    
+    }
 }
