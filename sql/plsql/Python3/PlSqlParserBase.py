9d0d06d6f4544807ce5b1cfbc52e8803a70c517c
Chris Cranford <chris@hibernate.org>
2025-10-19 17:12:10 -0400

    [PlSql] Multiple grammar improvements (#4640)
    * [PlSql] Implement "drop materialized view log" syntax
    
    * [PlSql] Implement "drop materialized view log" syntax test
    
    * [PlSql] Improve "create sequence"
    
    * [PlSql] Make UNUSABLE optional in on_comp_partitioned_clause
    
    * [PlSql] Add shrink_clause as an option index partition modifications
    
    * [PlSql] Add compute/estimate clauses to analyze
    
    * [PlSql] Support parallel degree/instances sub-clauses
    
    * [PlSql] Permit table_properties in any order
    
    * [PlSql] Change relational_property precedence order
    
    * [PlSql] Use constraint_name for partition by reference identifier
    
    * [PlSql] Add internal/external to table_partition_description
    
    * [PlSql] Allow physical properties in any order
    
    * [PlSql] Allow in-memory clause on partition_storage_clause
    
    This change also adjusts the behavior of inmemory_attributes so that the
    SQL can supply these attributes in any order, as allowed by the engine.
    
    * [PlSql] Add external table compression options
    
    * [PlSql] Remove redundant physical_attributes_clause rule
    
    * [PlSql] Support custom log groups on specific columns
    
    * [PlSql] Add unique index supplemental logging
    
    In Oracle 10g, supplemental logging was improved for logical replication
    to include the "UNIQUE INDEX" clause to log columns that are not declared
    as unique constraints, used by Streams and GoldenGate replication.
    
    * [PlSql] Improve truncate_table grammar options
    
    * [PlSql] Improve drop_table grammar options
    
    * [PlSql] Support alter table modify subpartition
    
    * [PlSql] Extend logical_replication_clause for Oracle 23
    
    * [PlSql] Allow unnamed varray storage
    
    * [PlSql] Permit quoted column types
    
    * [PlSql] Extend identity_options coverage
    
    * [PlSql] Improve virtual_column_definition rule
    
    * [PlSql] Support casting object types to relational tables
    
    * [PlSql] Support negative literal numeric values
    
    * [PlSql] Support character varying/sdo_geometry data types
    
    * [PlSql] Remove redundant regular_id E_LETTER rule
    
    * [PlSql] Add ENABLED regular_id keyword
    
    * [PlSql] Remove redundant regular_id ERROR_INDEX and ERROR_CODE rules
    
    * [PlSql] Allow columns named template_table and timezone
    
    * [PlSql] Improve object permissions
    
    * [PlSql] Support move partition/subpartition
    
    * [PlSql] Fix initial value for `_isVersion11` for Python3

--- sql/plsql/Python3/PlSqlParserBase.py
+++ sql/plsql/Python3/PlSqlParserBase.py
@@ -3,16 +3,23 @@ from antlr4 import *
 class PlSqlParserBase(Parser):
 
     _isVersion10 = False
+    _isVersion11 = True
     _isVersion12 = True
 
     def isVersion10(self):
         return self._isVersion10
+
+    def isVersion11(self):
+        return self._isVersion11
 
     def isVersion12(self):
         return self._isVersion12
 
     def setVersion10(self, value):
         self._isVersion10 = value
+
+    def setVersion11(self, value):
+        self._isVersion11 = value
 
     def setVersion12(self, value):
         self._isVersion12 = value
