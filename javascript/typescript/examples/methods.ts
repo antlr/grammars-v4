e42f4bd22ded5613c97342b5a8943b2a5f7893a9
TwoOfTwelve <github@twooftwelve.de>
2026-06-12 20:19:57 +0200

    Fixed errors in TypeScriptParser.g4 (#4892)
    * Fixed errors in TypeScriptParser.g4 by changing order of rules in 'statement'
    
    * Added test files
    
    ---------
    
    Co-authored-by: Alexander Milster <alexander.milster@gmx.de>

---
+++ javascript/typescript/examples/methods.ts
@@ -0,0 +1,11 @@
+function add1(a: number, b: number) {
+    return a + b;
+}
+
+const add2 = function (a: number, b: number) {
+    return a + b;
+}
+
+const add3 = (a: number, b: number) => {
+    return a + b;
+}
