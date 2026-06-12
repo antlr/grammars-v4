763b5819b9313e45ec460d06119b607c4eda3bd2
Briseglace <58014447+Briseglace@users.noreply.github.com>
2026-06-12 14:41:10 -0400

    [PlSql] Changes in merge_statement and unary_expression (#4883)
    - Added the optional static_returning_clause to merge_statement.
    - unary_expression :
        - Added support for OLD.
        - Removed DISTINCT and ALL since they are already implemented in more specific rules.
        - Divided unary_expression and unary_expression_core to avoid recursivity problems like OLD NEW column_name.
        - Moved + and - operators to model_expression to allow only one of them before unary_expression.
        - Removed TODOs from 2015 for readability.

--- sql/plsql/examples/avg_min_max.sql
+++ sql/plsql/examples/avg_min_max.sql
@@ -1,1 +1,3 @@
-SELECT AVG(column_name), MIN(column_name), MAX(column_name) FROM table_name
+SELECT AVG(column_name), MIN(column_name), MAX(column_name) FROM table_name;
+
+SELECT AVG(DISTINCT column_name), MIN(DISTINCT column_name), MAX(DISTINCT column_name) FROM table_name;
---
+++ sql/plsql/more-examples/static_returning_clause_invalid.sql
@@ -0,0 +1,1 @@
+update my_table set my_column = 1 returning old new my_column into l_column;
